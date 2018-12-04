a74636b44481be648044085a4ded477e0190477a
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-04 16:42:56 +1100

    Add files via upload

--- mk_austshape.sh
+++ mk_austshape.sh
@@ -1,1 +1,1 @@
-ogr2ogr hex_aust.shp 'hex_55km_layer_ESRI_Shapefile.vrt' -dialect sqlite -sql @austshape.sql
+ogr2ogr hex_57_aust.shp 'hex_57km_layer_ESRI_Shapefile.vrt' -dialect sqlite -sql @austshape.sql
