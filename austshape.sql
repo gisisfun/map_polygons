c9ed830f21ea228b608710139a1ece39e8e5e4b5
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-04 14:23:04 +1100

    Add files via upload

--- austshape.sql
+++ austshape.sql
@@ -1,1 +1,1 @@
-SELECT shapes.*, shapes.geometry FROM shapes, aust WHERE ST_Intersects(shapes.geometry,aust.geometry)
+SELECT shapes.*, st_area(shapes.geometry)*12391.3 as area, shapes.geometry FROM shapes, aust WHERE ST_Intersects(shapes.geometry,aust.geometry)
