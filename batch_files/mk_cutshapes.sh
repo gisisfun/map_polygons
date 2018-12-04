548a12ac936df3b7b26dd860726f60f5865caedf
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-05 09:58:06 +1100

    Update mk_cutshapes.sh

--- batch_files/mk_cutshapes.sh
+++ batch_files/mk_cutshapes.sh
@@ -1,1 +1,1 @@
-ogr2ogr cut_sa1.shp 'hex_aust_to_ref_layer_ESRI_Shapefile.vrt' -dialect sqlite -sql @cut_by_shape.sql
+ogr2ogr ../shapefiles/cut_sa1.shp '../vrt/hex_aust_to_ref_layer_ESRI_Shapefile.vrt' -dialect sqlite -sql @cut_by_shape.sql
