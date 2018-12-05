b438fd6d8eaa497667051ab96457a863fe6cf313
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-05 12:29:17 +1100

    Add files via upload

--- sql/cut_by_shape.sql
+++ sql/cut_by_shape.sql
@@ -1,1 +1,13 @@
-SELECT ref_g.SA1_MAIN16, shapes.*, st_area(ref_g.geometry)*12391.3 as ref_area, st_area(shapes.geometry)*12391.3 as shp_area, shapes.geometry FROM ref_g,shapes WHERE ST_Intersection(ref_g.geometry,shapes.geometry) or ST_Intersects(ref_g.geometry,shapes.geometry)or ST_Contains(ref_g.geometry,shapes.geometry) or ST_Intersects(ref_g.geometry,shapes.geometry) or ST_Within(ref_g.geometry,shapes.geometry) limit 100
+SELECT 
+ref_g.SA1_MAIN16, 
+shapes.p,  
+(ST_Area(ST_Intersection(ref_g.geometry,shapes.geometry))*12391.3)/(st_area(ref_g.geometry)*12391.3) as ref_prop, 
+st_area(shapes.geometry)*12391.3 as shp_area, 
+ST_Intersection(ref_g.geometry,shapes.geometry) 
+FROM ref_g,shapes 
+WHERE ST_Intersection(ref_g.geometry,shapes.geometry) or 
+ST_Intersects(ref_g.geometry,shapes.geometry) or 
+ST_Contains(ref_g.geometry,shapes.geometry) or 
+ST_Intersects(ref_g.geometry,shapes.geometry) or 
+ST_Within(ST_Centroid(ref_g.geometry),shapes.geometry) or 
+ST_Crosses(ref_g.geometry,shapes.geometry) limit 500
