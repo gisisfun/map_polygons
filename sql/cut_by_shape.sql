cef1816bbb5e137799fdaa93c96f39301bf3d085
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-05 16:38:38 +1100

    Add files via upload

--- sql/cut_by_shape.sql
+++ sql/cut_by_shape.sql
@@ -1,8 +1,8 @@
-SELECT 
-ref_g.SA1_MAIN16, 
+SELECT ref_g.SA1_MAIN16, 
 shapes.p,  
 (ST_Area(ST_Intersection(ref_g.geometry,shapes.geometry))*12391.3)/(st_area(ref_g.geometry)*12391.3) as ref_prop, 
 st_area(shapes.geometry)*12391.3 as shp_area, 
+'SA1_2016',
 ST_Intersection(ref_g.geometry,shapes.geometry) 
 FROM ref_g,shapes 
 WHERE ST_Intersection(ref_g.geometry,shapes.geometry) or 
@@ -10,4 +10,4 @@ ST_Intersects(ref_g.geometry,shapes.geometry) or
 ST_Contains(ref_g.geometry,shapes.geometry) or 
 ST_Intersects(ref_g.geometry,shapes.geometry) or 
 ST_Within(ST_Centroid(ref_g.geometry),shapes.geometry) or 
-ST_Crosses(ref_g.geometry,shapes.geometry) limit 500
+ST_Crosses(ref_g.geometry,shapes.geometry)
