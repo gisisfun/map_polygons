3ec9264ef254cecef9ed74e234894a3f7798ff2d
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-06 13:44:48 +1100

    Add files via upload

--- batch_files/mk_cutshapes.sh
+++ batch_files/mk_cutshapes.sh
@@ -1,1 +1,1 @@
-ogr2ogr ../shapefiles/cut_sa1.shp '../vrt/hex_aust_to_ref_layer_ESRI_Shapefile.vrt' -dialect sqlite -sql @cut_by_shape.sql
+ogr2ogr ../shapefiles/cut_sa1.shp '../vrt/poly_to_shape_ESRI_Shapefile.vrt' -dialect sqlite -sql @cut_by_shape.sql
