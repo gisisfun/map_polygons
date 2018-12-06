34058ec43489f53235614c9dbd4babad9671bbfa
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-06 13:45:38 +1100

    Add files via upload

--- sql/cut_by_shape.sql
+++ sql/cut_by_shape.sql
@@ -1,13 +1,13 @@
-SELECT ref_g.SA1_MAIN16, 
+SELECT poly.SA1_MAIN16, 
 shapes.p,  
-(ST_Area(ST_Intersection(ref_g.geometry,shapes.geometry))*12391.3)/(st_area(ref_g.geometry)*12391.3) as ref_prop, 
+(ST_Area(ST_Intersection(poly.geometry,shapes.geometry))*12391.3)/(st_area(poly.geometry)*12391.3) as ref_prop, 
 st_area(shapes.geometry)*12391.3 as shp_area, 
 'SA1_2016',
-ST_Intersection(ref_g.geometry,shapes.geometry) 
-FROM ref_g,shapes 
-WHERE ST_Intersection(ref_g.geometry,shapes.geometry) or 
-ST_Intersects(ref_g.geometry,shapes.geometry) or 
-ST_Contains(ref_g.geometry,shapes.geometry) or 
-ST_Intersects(ref_g.geometry,shapes.geometry) or 
-ST_Within(ST_Centroid(ref_g.geometry),shapes.geometry) or 
-ST_Crosses(ref_g.geometry,shapes.geometry)
+ST_Intersection(poly.geometry,shapes.geometry) 
+FROM poly,shapes 
+WHERE ST_Intersection(poly.geometry,shapes.geometry) or 
+ST_Intersects(poly.geometry,shapes.geometry) or 
+ST_Contains(poly.geometry,shapes.geometry) or 
+ST_Intersects(poly.geometry,shapes.geometry) or 
+ST_Within(ST_Centroid(poly.geometry),shapes.geometry) or 
+ST_Crosses(poly.geometry,shapes.geometry)
