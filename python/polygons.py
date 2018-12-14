0ea79536989d2712400d87a92fd5c5fed2b8cf7b
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-14 22:25:28 +1100

    Add files via upload

--- python/polygons.py
+++ python/polygons.py
@@ -181,16 +181,16 @@ def ref_files():
         url = 'http://www.abs.gov.au/AUSSTATS/subscriber.nsf/log?openagent&1270055001_aus_2016_aust_shape.zip&1270.0.55.001&Data%20Cubes&5503B37F8055BFFECA2581640014462C&0&July%202016&24.07.2017&Latest'
         urllib.request.urlretrieve(url, '../shapefiles/1270055001_aus_2016_aust_shape.zip')
         print('Unzipping ABS Australia file in Shape file format')    
-        Archive('../shapefiles/1270055001_aus_2016_aust_shape.zip').extractall(dir_path)
+        Archive('../shapefiles/1270055001_aus_2016_aust_shape.zip').extractall('../shapefiles')
     else:
         print('ABS Australia file in Shape file format exists')
     
     if not os.path.isfile('AUS_2016_AUST.tab'):
         print('Downloading ABS Australia file in Tab file format')
         url ='http://www.abs.gov.au/AUSSTATS/subscriber.nsf/log?openagent&1270055001_aus_2016_aust_tab.zip&1270.0.55.001&Data%20Cubes&F18065BF058615F9CA2581640014491B&0&July%202016&24.07.2017&Latest'
-        urllib.request.urlretrieve(url, './shapefiles/1270055001_aus_2016_aust_tab.zip') 
+        urllib.request.urlretrieve(url, '../shapefiles/1270055001_aus_2016_aust_tab.zip') 
         print('Unzipping ABS Australia file in Tab file format')
-        Archive('../shapefiles/1270055001_aus_2016_aust_tab.zip').extractall(dir_path)
+        Archive('../shapefiles/1270055001_aus_2016_aust_tab.zip').extractall('../tabfiles')
     else:
         print('ABS Australia file in Tab file format exists')  
 
