b63592353ce3290989130d0e958a092ab240ab19
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-14 14:17:37 +1100

    Update feat_aust.sql

--- sql/feat_aust.sql
+++ sql/feat_aust.sql
@@ -3,15 +3,15 @@ SELECT
 'SA1_2016' as Feat_Src,
 Feat.SA1_MAIN16 as Feat_Code,
 '' as Feat_Name,
-Shapes.p as Poly,  
-(ST_Area(ST_Intersection(Feat.geometry,Shapes.geometry))*12391.3)/(st_area(Feat.geometry)*12391.3) as Feat_Prop, 
-(ST_Area(ST_Intersection(Feat.geometry,Shapes.geometry))*12391.3)/(st_area(Shapes.geometry)*12391.3) as Shp_Prop, 
-st_area(Shapes.geometry)*12391.3 as Shp_Area, 
-ST_Intersection(Feat.geometry,Shapes.geometry) 
+Aust.p as Poly,  
+(ST_Area(ST_Intersection(Feat.geometry,Aust.geometry))*12391.3)/(st_area(Feat.geometry)*12391.3) as Feat_Prop, 
+(ST_Area(ST_Intersection(Feat.geometry,Aust.geometry))*12391.3)/(st_area(Aust.geometry)*12391.3) as Shp_Prop, 
+st_area(Aust.geometry)*12391.3 as Shp_Area, 
+ST_Intersection(Feat.geometry,Aust.geometry) 
 FROM Feat,Shapes 
-WHERE ST_Intersection(Feat.geometry,Shapes.geometry) or 
-ST_Intersects(Feat.geometry,Shapes.geometry) or 
-ST_Contains(Feat.geometry,Shapes.geometry) or 
-ST_Intersects(Feat.geometry,Shapes.geometry) or 
-ST_Within(ST_Centroid(Feat.geometry),Shapes.geometry) or 
-ST_Crosses(Feat.geometry,Shapes.geometry)
+WHERE ST_Intersection(Feat.geometry,Aust.geometry) or 
+ST_Intersects(Feat.geometry,Aust.geometry) or 
+ST_Contains(Feat.geometry,Aust.geometry) or 
+ST_Intersects(Feat.geometry,Aust.geometry) or 
+ST_Within(ST_Centroid(Feat.geometry),Aust.geometry) or 
+ST_Crosses(Feat.geometry,Aust.geometry)
