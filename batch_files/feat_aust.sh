fe09f1ee9e287578db0b9c41eba371cb5ee1b169
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-14 13:45:43 +1100

    Update feat_aust.sh

--- batch_files/feat_aust.sh
+++ batch_files/feat_aust.sh
@@ -1,1 +1,1 @@
-ogr2ogr feat_shape_75km_sa1.shp 'feat_aust.vrt' -dialect sqlite -sql @feat_aust.sql
+ogr2ogr ../shapefiles/feat_shape_75km_sa1.shp '../shapefiles/feat_aust.vrt' -dialect sqlite -sql @../sql/feat_aust.sql
