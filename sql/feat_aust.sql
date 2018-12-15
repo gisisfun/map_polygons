191e59c24721de79fedbd5859bf83e5a9bec53c9
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-15 22:20:29 +1100

    Update feat_aust.sql

--- sql/feat_aust.sql
+++ sql/feat_aust.sql
@@ -1,7 +1,7 @@
 SELECT 
  'P' as Feat_Type,
-'SA1_2016' as Feat_Src,
-Feat.SA1_MAIN16 as Feat_Code,
+'SA1_2011' as Feat_Src,
+Feat.SA1_7DIG11 as Feat_Code,
 '' as Feat_Name,
 Aust.p as Poly,  
 (ST_Area(ST_Intersection(Feat.geometry,Aust.geometry))*12391.3)/(st_area(Feat.geometry)*12391.3) as Feat_Prop, 
