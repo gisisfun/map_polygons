5c667962f2ce02b9fbe584d18ff839339a28e365
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-16 23:51:12 +1100

    Update agg_donor.sql

--- sql/agg_donor.sql
+++ sql/agg_donor.sql
@@ -1,15 +1,7 @@
-SELECT 
- 'P' as Feat_Type,
-'SA1_2016' as Feat_Src,
-Feat.SA1_MAIN16 as Feat_Code,
-'' as Feat_Name,
-Aust.p as Poly,  
-(ST_Area(ST_Intersection(Feat.geometry,Aust.geometry))*12391.3)/(st_area(Feat.geometry)*12391.3) as Feat_Prop, 
-(ST_Area(ST_Intersection(Feat.geometry,Aust.geometry))*12391.3)/(st_area(Aust.geometry)*12391.3) as Shp_Prop, 
-st_area(Aust.geometry)*12391.3 as Shp_Area, 
-ST_Intersection(Feat.geometry,Aust.geometry) 
-FROM Feat,Aust
-WHERE ST_Intersection(Feat.geometry,Aust.geometry) or 
-ST_Intersects(Feat.geometry,Aust.geometry) or 
-ST_Contains(Feat.geometry,Aust.geometry) or 
-ST_Crosses(Feat.geometry,Aust.geometry)
+SELECT Donor.Poly,  
+Sum(Donor.NeedAssist), 
+Sum(Donor.TotP), 
+Aust.geometry
+FROM Donor,Aust
+WHERE Donor.Poly=Aust.Poly
+GROUP BY Donor.Poly
