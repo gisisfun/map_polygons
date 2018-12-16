216833cc819848dd4979600d6953dfc792392559
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-16 16:33:53 +1100

    Update donor_feat.sql

--- sql/donor_feat.sql
+++ sql/donor_feat.sql
@@ -2,6 +2,6 @@ SELECT Feat.Poly,Feat.Feat_Src,
 Feat.Feat_Code,
 (Donor.Persons_Total_Has_need_for_assistance*Feat.Feat_Prop) as NeedAssist,
 (Donor.Persons_Total_Total*Feat.Feat_Prop) as TotP,
-Feat.geography
+Feat.geometry
 FROM Donor,Feat
 WHERE Donor.region_id=Feat.Feat_Code and Feat.Feat_Src='SA1_2011'
