b458ba601856e60a757f4bac19964887d247c033
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-16 13:06:02 +1100

    Update donor_feat.sql

--- sql/donor_feat.sql
+++ sql/donor_feat.sql
@@ -1,10 +1,7 @@
-SELECT 
- Feat.Poly,
-'SA1_2016' as Feat_Src,
-Feat.SA1_MAIN16 as Feat_Code,
-'' as Feat_Name,
-Feat.Poly,  
-(ST_Area(ST_Intersection(Feat.geometry,A
-feat.geography
+SELECT Feat.Poly,
+Feat.SA1_7DIG11 as Feat_Code,
+(Donor.Persons_Total_Has_need_for_assistance*Feat.Feat_Prop) as NeedAssist,
+(Donor.Persons_Total_Total*Feat.Feat_Prop) as TotP,
+Feat.geography
 FROM Donor,Feat
-WHERE Donor.SA1_MAINCODE_2011=Feat.Feat.Code and Feat_Src='SA1_2011'
+WHERE Donor.region_id=Feat.Feat_Code and Feat.Feat_Src='SA1_2011'
