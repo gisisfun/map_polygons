c75e8445ac25a06198731f2480836872a52ae2ed
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-19 03:18:09 +0000

    Update donor_aust.sh

--- batch_files/donor_aust.sh
+++ batch_files/donor_aust.sh
@@ -1,3 +1,3 @@
 #Persons_Total_Has_need_for_assistance/Persons_Total_Total by region_id SA1_7DigitCODE_2011
 
-ogr2ogr ../shapefiles/donor_aust_57km_aust.shp '../vrt/donor_aust.vrt' -dialect sqlite -sql @../sql/donor_aust.sql
+ogr2ogr ../shapefiles/donor_aust_SA1_aust.shp '../vrt/donor_aust.vrt' -dialect sqlite -sql @../sql/donor_aust.sql
