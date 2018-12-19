2481a7903eec4a0128840b7d5218185352bfba80
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-19 04:06:14 +0000

    Update donor_src.sql

--- sql/donor_src.sql
+++ sql/donor_src.sql
@@ -3,4 +3,4 @@ Donor.Persons_Total_Has_Need_For_Assistance as NeedAssist,
 Donor.Persons_Total_Total asTotP,
 Src.geometry
 FROM Donor,Src
-WHERE Src.Sa1_7DIG11=Donor.region_id
+WHERE Src.SA1_7DIG11=Donor.region_id
