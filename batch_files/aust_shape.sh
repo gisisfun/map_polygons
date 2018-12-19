9a21817e7ed3a05a04e27966a60b24884ba41892
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-19 23:42:14 +0000

    Update aust_shape.sh

--- batch_files/aust_shape.sh
+++ batch_files/aust_shape.sh
@@ -1,1 +1,2 @@
 ogr2ogr ../shapefiles/aust_hex_shape_57km.shp '../vrt/aust_shape.vrt' -dialect sqlite -sql @../sql/aust_shape.sql
+ogr2ogr -f "CSV" ../csv/aust_hex_shape_57km.csv ../shapefiles/aust_hex_shape_57km.shp
