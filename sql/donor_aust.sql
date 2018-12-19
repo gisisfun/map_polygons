d7c48f828119409576dd52cf184e209dc00f7c64
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-19 03:27:07 +0000

    Update donor_aust.sql

--- sql/donor_aust.sql
+++ sql/donor_aust.sql
@@ -1,5 +1,6 @@
 SELECT Feat.SA1_7DIG11, 
 Donor.Persons_Total_Has_Need_For_Assistance as NeedAssist,
 Donor.Persons_Total_Total asTotP,
+Feat.geometry
 FROM Feat,Aust 
 WHERE ST_Intersects(Feat.geometry,Aust.geometry) 
