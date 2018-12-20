016bc334cf27fec057341196ccedadaf1cb4bf25
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-20 03:29:16 +0000

    Update feat_aust.sql

--- sql/feat_aust.sql
+++ sql/feat_aust.sql
@@ -7,5 +7,6 @@ Aust.p as Poly,
 (ST_Area(ST_Intersection(Feat.geometry,Aust.geometry))*12391.3)/(st_area(Feat.geometry)*12391.3) as Feat_Prop,  
 ST_Intersection(Feat.geometry,Aust.geometry) 
 FROM Feat,Aust 
-WHERE ST_Intersects(Feat.geometry,Aust.geometry) and Feat_Prop is not NULL
-ORDER by POLY
+WHERE ST_Intersects(Feat.geometry,Aust.geometry) 
+and Feat_Prop is not NULL
+ORDER by Poly
