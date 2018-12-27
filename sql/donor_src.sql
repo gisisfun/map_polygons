94de0d6b2832aa7ae508d8e13452593fac8883c3
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-28 10:52:27 +1100

    Delete donor_src.sql

--- sql/donor_src.sql
+++
@@ -1,6 +0,0 @@
-SELECT Src.SA1_7DIG11, Src.SA1_MAIN11, 
-Donor.Persons_Total_Has_Need_For_Assistance as NeedAssist,
-Donor.Persons_Total_Total asTotP,
-Src.geometry
-FROM Donor,Src
-WHERE Src.SA1_7DIG11=Donor.region_id
