5d994e7170a202a4a380ae1b500309f31afecccc
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-29 12:22:07 +1100

    Add files via upload

--- sql/shape_11_16.sql
+++ sql/shape_11_16.sql
@@ -2,7 +2,8 @@ SELECT Shape11.Poly,
 Shape11.NeedAssist as NeedA11,
 Shape11.TotP as ToTP11,
 Shape16.NeedAssist as NeedA16,
-Shape16.TotP as ToTP16
+Shape16.TotP as ToTP16,
+Shape11.geometry
 FROM Shape11,Shape16
 WHERE Shape11.Poly=Shape16.Poly
 
