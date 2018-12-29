3119304eb462d4f38d8f28f68104cd3a691aba15
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-29 12:23:57 +1100

    Delete donor_src.sh

--- batch_files/donor_src.sh
+++
@@ -1,3 +0,0 @@
-#Persons_Total_Has_need_for_assistance/Persons_Total_Total by region_id SA1_7DigitCODE_2011
-
-ogr2ogr ../shapefiles/donor_aust_SA1_aust.shp '../vrt/donor_src.vrt' -dialect sqlite -sql @../sql/donor_src.sql
