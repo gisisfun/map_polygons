b889bf5c2b8d9a97427279c08fdb71971db83db8
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-29 12:23:40 +1100

    Delete donor_feat.sh

--- batch_files/donor_feat.sh
+++
@@ -1,3 +0,0 @@
-#Persons_Total_Has_need_for_assistance/Persons_Total_Total by region_id SA1_7DigitCODE_2011
-
-ogr2ogr ../shapefiles/donor_feat_57km_aust.shp '../vrt/donor_feat.vrt' -dialect sqlite -sql @../sql/donor_feat.sql
