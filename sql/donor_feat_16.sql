b618a9f1786c57255c787f4cf0a79a3a1ad0bd0a
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-31 07:11:13 +1100

    Update donor_feat_16.sql

--- sql/donor_feat_16.sql
+++ sql/donor_feat_16.sql
@@ -1,9 +1,7 @@
 SELECT Feat.Poly,Feat.Feat_Src,
 Feat.Feat_Code,
-
-
-(Donor_SA1.P_Tot_Need_for_assistance*Feat.Feat_Prop) as NeedAssist,
-(Donor_SA1.P_Tot_Tot*Feat.Feat_Prop) as TotP,
+(Donor_G18.P_Tot_Need_for_assistance*Feat.Feat_Prop) as NeedAssist,
+(Donor_G18.P_Tot_Tot*Feat.Feat_Prop) as TotP,
 Feat.geometry
-FROM Donor_SA1,Feat
-WHERE Donor_SA1.Feat_Code=Feat.Feat_Code
+FROM Donor_G18,Feat
+WHERE Donor_G18.Feat_Code=Feat.Feat_Code
