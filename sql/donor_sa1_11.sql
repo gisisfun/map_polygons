abdcb991cdf7bf67a6ffbb35479a4d617733a2d0
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-31 07:12:04 +1100

    Delete donor_sa1_11.sql

--- sql/donor_sa1_11.sql
+++
@@ -1,7 +0,0 @@
-SELECT Donor_B18.region_id as Feat_Code,
-(Donor_B21.Persons_Total_Provided_unpaid_assistance) as UnpAssist,
-(Donor_B21.Persons_Total_Total) as UnpAssTot,
-(Donor_B18.Persons_Total_Has_need_for_assistance) as NeedAssist,
-(Donor_B18.Persons_Total_Total) as TotP
-FROM Donor_B18,Donor_B21
-WHERE Donor_B18.region_id=Donor_B21.region_id
