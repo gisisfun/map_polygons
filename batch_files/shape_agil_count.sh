cf366443ddcd6c2941cb792bc9ac5bf193e0f65e
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-03 20:01:30 +1100

    Add files via upload

---
+++ batch_files/shape_agil_count.sh
@@ -0,0 +1,2 @@
+ogr2ogr ../shapefiles/shape_donor_feat_57km_agil_count.shp '../vrt/shape_agil.vrt' -dialect sqlite -sql @../sql/shape_agil_count.sql
+#ogr2ogr -f "CSV" ../csv/shqpe_donor_feat_75km_aust_11.csv ../shapefiles/shape_donor_feat_57km_aust_11.shp
