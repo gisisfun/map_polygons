ff537b620f6de824965aadcfa48ea0dbb7645e43
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-03 19:12:26 +1100

    Add files via upload

---
+++ sql/shape_11_16_place.sql
@@ -0,0 +1,10 @@
+SELECT Shape11_16.Poly,
+Shape11_16.NeedA11,
+Shape11_16.ToTP11,
+Shape11_16.NeedA16,
+Shape11_16.ToTP16,
+Place.places, 
+Shape11_16.geometry
+FROM Shape11_16 LEFT JOIN Place
+ON Shape11_16.Poly = Place.Poly
+
