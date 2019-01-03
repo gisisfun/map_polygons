368864aa10aa7916383944c3b1d08fc46c2fd914
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-03 14:19:46 +1100

    Add files via upload

---
+++ batch_files/shape_place_list.sh
@@ -0,0 +1,2 @@
+ogr2ogr ../shapefiles/shape_donor_feat_57km_place_list.shp '../vrt/shape_place.vrt' -dialect sqlite -sql @../sql/shape_place_list.sql
+#ogr2ogr -f "CSV" ../csv/shqpe_donor_feat_75km_aust_11.csv ../shapefiles/shape_donor_feat_57km_aust_11.shp
