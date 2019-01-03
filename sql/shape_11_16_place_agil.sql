865d639cb9724c5c19ca35dca6051dc287d549da
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-03 20:09:56 +1100

    Add files via upload

---
+++ sql/shape_11_16_place_agil.sql
@@ -0,0 +1,11 @@
+SELECT Shape11_16_place.Poly,
+Shape11_16_place.NeedA11,
+Shape11_16_place.ToTP11,
+Shape11_16_place.NeedA16,
+Shape11_16_place.ToTP16,
+Shape11_16_place.places,
+AGIL.AGILplaces,
+Shape11_16_place.geometry
+FROM Shape11_16_place LEFT JOIN AGIL
+ON Shape11_16_place.Poly = AGIL.Poly
+
