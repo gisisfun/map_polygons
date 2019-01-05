1c497946de05f34bafe9d8972698dea85fe4fc11
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-05 13:29:18 +1100

    Add files via upload

---
+++ batch_files/shape_road_count.sh
@@ -0,0 +1,2 @@
+ogr2ogr ../shapefiles/shape_donor_feat_57km_road_count.shp '../vrt/shape_road.vrt' -dialect sqlite -sql @../sql/shape_road_count.sql
+#ogr2ogr -f "CSV" ../csv/shqpe_donor_feat_75km_aust_11.csv ../shapefiles/shape_donor_feat_57km_aust_11.shp
