448b6d68f36056a383eff4872b6954ddb55f6b54
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-09 13:55:06 +1100

    Update dbcsvfun.sh

--- batch_files/dbcsvfun.sh
+++ batch_files/dbcsvfun.sh
@@ -1,6 +1,6 @@
 
-sed -e "s#changeme#${1}#g" ../spatialite_db/csvcmds_tmpl >../spatialite_db/cmds
-cat ../spatialite_db/cmds | spatialite ../spatialite_db/db.sqlite
+sed -e "s#changeme#${1}#g" ../spatialite_db/csvcmds_tmpl >cmds.sh
+sh cmds.sh
 
 
 
