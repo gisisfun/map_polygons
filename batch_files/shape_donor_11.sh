5cbc32b06008082b90c34e778b52fd8c35890773
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-18 15:49:33 +1100

    Delete shape_donor_11.sh

--- batch_files/shape_donor_11.sh
+++
@@ -1,2 +0,0 @@
-ogr2ogr ../shapefiles/shape_donor_feat_57km_aust_11.shp '../vrt/shape_donor_11.vrt' -dialect sqlite -sql @../sql/shape_donor_11.sql
-ogr2ogr -f "CSV" ../csv/shqpe_donor_feat_75km_aust_11.csv ../shapefiles/shape_donor_feat_57km_aust_11.shp
