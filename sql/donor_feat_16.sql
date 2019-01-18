8420c2499a417b36557b279cbc96ecac6f4b3071
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-18 13:47:20 +1100

    Delete donor_feat_16.sql

--- sql/donor_feat_16.sql
+++
@@ -1,7 +0,0 @@
-SELECT Feat.Poly,Feat.Feat_Src,
-Feat.Feat_Code,
-(Donor_G18.P_Tot_Need_for_assistance*Feat.Feat_Prop) as NeedAssist,
-(Donor_G18.P_Tot_Tot*Feat.Feat_Prop) as TotP,
-Feat.geometry
-FROM Donor_G18,Feat
-WHERE Donor_G18.Feat_Code=Feat.Feat_Code
