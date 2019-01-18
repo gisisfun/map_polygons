78da5f808530ad3412b32e680fcc39ccda539523
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-18 13:46:18 +1100

    Add files via upload

---
+++ batch_files/donor_feat_16_G18.sh
@@ -0,0 +1,1 @@
+ogr2ogr ../shapefiles/donor_feat_57km_aust_16_G18.shp '../vrt/donor_feat_16.vrt' -dialect sqlite -sql @../sql/donor_feat_16_G18.sql
---
+++ batch_files/donor_feat_16_G21.sh
@@ -0,0 +1,1 @@
+ogr2ogr ../shapefiles/donor_feat_57km_aust_16_G21.shp '../vrt/donor_feat_16.vrt' -dialect sqlite -sql @../sql/donor_feat_16_G21.sql
