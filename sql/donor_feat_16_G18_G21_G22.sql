1676cd4a870cce321e16039331364c17ded77e46
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-22 13:48:41 +1100

    Update donor_feat_16_G18_G21_G22.sql

--- sql/donor_feat_16_G18_G21_G22.sql
+++ sql/donor_feat_16_G18_G21_G22.sql
@@ -5,9 +5,9 @@ round(sum(Donor_G21.P_Tot_prvided_unpaid_assist*Feat.Feat_Prop),1) as PUnPA16,
 round(sum(Donor_G21.P_Tot_Tot*Feat.Feat_Prop),1) as PUnPAT16,
 round(sum(Donor_G22.P_Tot_CF_Total*Feat.Feat_Prop),1) as PUnPCC11,
 round(sum(Donor_G22.P_Tot_Total*Feat.Feat_Prop),1) as PUnPCCT11
-FROM Donor_G18,Donor_B21,Donor_B22,Feat
-WHERE Donor_G18.region_id=Feat.Feat_Code 
-and Donor_G21.region_id=Feat.Feat_Code  
-and Donor_G22.region_id=Feat.Feat_Code 
+FROM Feat 
+LEFT JOIN Donor_G18 ON Donor_G18.region_id=Feat.Feat_Code  
+LEFT JOIN Donor_G21 ON Donor_G21.region_id=Feat.Feat_Code 
+LEFT JOIN Donor_G22 ON Donor_G22.region_id=Feat.Feat_Code 
 GROUP BY Feat.Poly
 ORDER BY Feat.Poly
