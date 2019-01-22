8e29cbce4b2f43da95e889b1ad2628bea08161fb
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-22 12:14:59 +1100

    Add files via upload

---
+++ batch_files/donor_feat_16_G18_G21_G22.sql
@@ -0,0 +1,14 @@
+SELECT Feat.Poly,Feat.Feat_Src,
+Feat.Feat_Code,
+round(sum(Donor_G18.P_Tot_Need_for_assistance*Feat.Feat_Prop),1) as NeedA16,
+round(sum(Donor_G18.P_Tot_Tot*Feat.Feat_Prop),1) as NeedAT16,
+round(sum(Donor_G21.P_Tot_prvided_unpaid_assist*Feat.Feat_Prop),1) as PUnPA16,
+round(sum(Donor_G21.P_Tot_Tot*Feat.Feat_Prop),1) as PUnPAT16,
+round(sum(Donor_G22.P_Tot_CF_Total*Feat.Feat_Prop),1) as PUnPCC11,
+round(sum(Donor_G22.P_Tot_Total*Feat.Feat_Prop),1) as PUPCCT11
+FROM Donor_G18,Donor_B21,Donor_B22,Feat
+WHERE Donor_G18.region_id=Feat.Feat_Code 
+and Donor_G18.region_id=Donor_G21.region_id 
+and Donor_G21.region_id=Donor_G22.region_id
+GROUP BY Feat.Poly
+ORDER BY Feat.Poly
