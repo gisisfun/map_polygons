14ca7ef06c8e2adf0c09432a1613ce0f9a107337
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-22 12:47:21 +1100

    Update donor_feat_11_B18_B21_B22.sql

--- sql/donor_feat_11_B18_B21_B22.sql
+++ sql/donor_feat_11_B18_B21_B22.sql
@@ -7,7 +7,7 @@ round(sum(Donor_B22.Persons_Total_Cared_for_Own_child_children_and_other_child_c
 round(sum(Donor_B22.Persons_Total_Total*Feat.Feat_Prop),1) as PUPCCT11
 FROM Donor_B18,Donor_B21,Donor_B22,Feat
 WHERE Donor_B18.region_id=Feat.Feat_Code 
-and Donor_B18.region_id=Donor_B21.region_id 
-and Donor_B18.region_id=Donor_B22.region_id
+and Donor_B21.region_id=Feat.Feat_Code
+and Donor_B22.region_id=Feat.Feat_Code
 GROUP BY Feat.Poly
 ORDER BY Feat.Poly
