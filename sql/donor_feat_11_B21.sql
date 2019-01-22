c9778f4248f347c0681ccd77257ba1c1c8df687d
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-22 11:58:06 +1100

    Delete donor_feat_11_B21.sql

--- sql/donor_feat_11_B21.sql
+++
@@ -1,7 +0,0 @@
-SELECT Feat.Poly,Feat.Feat_Src,
-Feat.Feat_Code,
-(Donor_B21.Persons_Total_Provided_unpaid_assistance*Feat.Feat_Prop) as PUnPAssist,
-(Donor_B21.Persons_Total_Total*Feat.Feat_Prop) as TotP,
-Feat.geometry
-FROM Donor_B21,Feat
-WHERE Donor_B21.region_id=Feat.Feat_Code
