bb3a8f9fa95602e7003769e4e91ace8558db1238
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-22 11:54:00 +1100

    Add files via upload

---
+++ batch_files/donor_feat_11_B18_B21_B22_csv.sh
@@ -0,0 +1,3 @@
+#Persons_Total_Has_need_for_assistance/Persons_Total_Total by region_id SA1_7DigitCODE_2011
+
+ogr2ogr ../csv/donor_feat_57km_11_B18_B21_B22.csv '../vrt/donor_feat_11.vrt' -dialect sqlite -sql @../sql/donor_feat_11_B18_B21_B22.sql
