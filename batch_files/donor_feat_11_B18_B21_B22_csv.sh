919b6333c38a5d6d5086a60e1c8b7053f4114172
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-22 12:29:47 +1100

    Update donor_feat_11_B18_B21_B22_csv.sh

--- batch_files/donor_feat_11_B18_B21_B22_csv.sh
+++ batch_files/donor_feat_11_B18_B21_B22_csv.sh
@@ -1,3 +1,1 @@
-#Persons_Total_Has_need_for_assistance/Persons_Total_Total by region_id SA1_7DigitCODE_2011
-
 ogr2ogr -f "CSV" ../csv/donor_feat_57km_11_B18_B21_B22.csv '../vrt/donor_feat_11.vrt' -dialect sqlite -sql @../sql/donor_feat_11_B18_B21_B22.sql
