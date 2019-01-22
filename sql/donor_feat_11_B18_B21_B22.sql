686c7f2d0cc3a23190e80bb155b007f7df6a74ea
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-22 12:40:03 +1100

    Update donor_feat_11_B18_B21_B22.sql

--- sql/donor_feat_11_B18_B21_B22.sql
+++ sql/donor_feat_11_B18_B21_B22.sql
@@ -1,5 +1,4 @@
-SELECT Feat.Poly,Feat.Feat_Src,
-Feat.Feat_Code,
+SELECT Feat.Poly,
 round(sum(Donor_B18.Persons_Total_Has_need_for_assistance*Feat.Feat_Prop),1) as NeedA11,
 round(sum(Donor_B18.Persons_Total_Total*Feat.Feat_Prop),1) as NeedAT11,
 round(sum(Donor_B21.Persons_Total_Provided_unpaid_assistance*Feat.Feat_Prop),1) as PUnPA11,
