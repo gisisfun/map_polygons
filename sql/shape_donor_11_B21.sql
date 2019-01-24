b1051a438f290af658c6fe1a6291db9718a43334
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-24 13:00:23 +1100

    Delete shape_donor_11_B21.sql

--- sql/shape_donor_11_B21.sql
+++
@@ -1,7 +0,0 @@
-SELECT Donor_Feat_B21.Poly,
-Sum(Donor_Feat_B21.PUnPAssist) as PUnPAssist,
-Sum(Donor_Feat_B21.TotP) as ToTP,
-Shape.geometry
-FROM Shape,Donor_Feat_B21
-WHERE Donor_Feat_B21.Poly=Shape.p
-GROUP BY Donor_Feat_B21.Poly
