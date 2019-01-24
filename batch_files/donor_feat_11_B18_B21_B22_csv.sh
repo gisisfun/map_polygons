0e49f9055faf6ce56145eba74ec73bdabf64e029
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-25 10:53:31 +1100

    Add files via upload

--- batch_files/donor_feat_11_B18_B21_B22_csv.sh
+++ batch_files/donor_feat_11_B18_B21_B22_csv.sh
@@ -1,1 +1,8 @@
-ogr2ogr -f "CSV" ../csv/donor_feat_57km_11_B18_B21_B22.csv '../vrt/donor_feat_11.vrt' -dialect sqlite -sql @../sql/donor_feat_11_B18_B21_B22.sql
+#Persons_Total_Has_need_for_assistance/Persons_Total_Total by region_id SA1_7DigitCODE_2011
+
+#ogr2ogr -f "CSV" ../csv/donor_feat_57km_11_B18_B21_B22.csv '../vrt/donor_feat_11.vrt' -dialect sqlite -sql @../sql/donor_feat_11_B18_B21_B22.sql
+sh dbcsvfun.sh 2011Census_B18_AUST_SA1_long
+sh dbcsvfun.sh 2011Census_B21_AUST_SA1_long
+sh dbcsvfun.sh 2011Census_B22_AUST_SA1_long
+sh dbcsvfun.sh feat_aust_57km_sa1_11
+spatialite ../spatialite_db/db.sqlite < ../spatialite_db/donor_feat_57km_16_G18_G21_G22.txt
--- batch_files/donor_feat_16_G18_G21_G22_csv.sh
+++ batch_files/donor_feat_16_G18_G21_G22_csv.sh
@@ -1,3 +1,6 @@
-#Persons_Total_Has_need_for_assistance/Persons_Total_Total by region_id SA1_7DigitCODE_2011
-
-ogr2ogr ../csv/donor_feat_57km_16_G18_G21_G22.csv '../vrt/donor_feat_16.vrt' -dialect sqlite -sql @../sql/donor_feat_16_G18_G21_G22.sql
+#ogr2ogr ../csv/donor_feat_57km_16_G18_G21_G22.csv '../vrt/donor_feat_16.vrt' -dialect sqlite -sql @../sql/donor_feat_16_G18_G21_G22.sql
+sh dbcsvfun.sh 2016Census_G18_AUS_SA1
+sh dbcsvfun.sh 2016Census_G21_AUS_SA1
+sh dbcsvfun.sh 2016Census_G22_AUS_SA1
+sh dbcsvfun.sh feat_aust_57km_sa1_16
+spatialite ../spatialite_db/db.sqlite < ../spatialite_db/donor_feat_57km_16_G18_G21_G22.txt
