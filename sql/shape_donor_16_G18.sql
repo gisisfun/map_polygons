2a4d398f64efbdb989df98ca33389bd2ca10af07
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-24 13:00:55 +1100

    Delete shape_donor_16_G18.sql

--- sql/shape_donor_16_G18.sql
+++
@@ -1,7 +0,0 @@
-SELECT Donor_Feat_G18.Poly,
-Sum(Donor_Feat_G18.NeedAssist) as NeedAssist,
-Sum(Donor.TotP) as ToTP,
-Shape.geometry
-FROM Shape,Donor_Feat_G18
-WHERE Donor_Feat_G18.Poly=Shape.p
-GROUP BY Donor_Feat_G18.Poly
