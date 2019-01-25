d56519b2d3149aec0ac92a7d5ca859edb4ad7d9c
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-25 21:30:24 +1100

    Delete shape_11_16_place_agil.sh

--- batch_files/shape_11_16_place_agil.sh
+++
@@ -1,2 +0,0 @@
-ogr2ogr ../shapefiles/shape_75km_11_16_place_agil.shp '../vrt/shape_11_16.vrt' -dialect sqlite -sql @../sql/shape_11_16_place_agil.sql
-#ogr2ogr -f "CSV" ../csv/shape_75km_11_16.csv ../shapefiles/shape_75km_11_16.shp
