af06d44eec05d55a0a463e918a499ef9a1e522cf
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-25 13:06:42 +1100

    Update shape_11_16.sql

--- sql/shape_11_16.sql
+++ sql/shape_11_16.sql
@@ -1,10 +1,21 @@
-SELECT Shape11.Poly,
-Shape11.NeedAssist as NeedA11,
-Shape11.TotP as ToTP11,
-Shape16.NeedAssist as NeedA16,
-Shape16.TotP as ToTP16,
+SELECT CAST(Shape11.Poly as INT) as Poly,
+Shape11.NeedA11,
+Shape11.NeedAT11,
+Shape11.PUnPA11,
+Shape11.PUnPAT11,
+Shape11.PUnPCC11,
+Shape11.PUnPCCT11,
+Shape16.NeedA16,
+Shape16.NeedAT16,
+Shape16.PUnPA16,
+Shape16.PUnPAT16,
+Shape16.PUnPCC16,
+Shape16.PUnPCCT16,
 Shape.est_area,
-Shape11.geometry
-FROM Shape11,Shape16,Shape
-WHERE Shape11.Poly=Shape16.Poly and Shape11.Poly=Shape.p
+Shape.geometry
+FROM Shape 
+LEFT JOIN Shape11
+ON Shape.p=CAST(Shape11.Poly as INT)
+LEFT JOIN Shape16
+ON Shape.p=CAST(Shape16.Poly as INT)
 
