e337cf0d3458d51e3718d95dd99045c19c0387fc
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-25 12:42:23 +1100

    Update donor_feat_57km_16_G18_G21_G22.sql

--- spatialite_db/donor_feat_57km_16_G18_G21_G22.sql
+++ spatialite_db/donor_feat_57km_16_G18_G21_G22.sql
@@ -4,8 +4,8 @@ round(sum(Donor_G18.P_Tot_Need_for_assistance*Feat.Feat_Prop),1) as NeedA16,
 round(sum(Donor_G18.P_Tot_Tot*Feat.Feat_Prop),1) as NeedAT16,
 round(sum(Donor_G21.P_Tot_prvided_unpaid_assist*Feat.Feat_Prop),1) as PUnPA16,
 round(sum(Donor_G21.P_Tot_Tot*Feat.Feat_Prop),1) as PUnPAT16,
-round(sum(Donor_G22.P_Tot_CF_Total*Feat.Feat_Prop),1) as PUnPCC11,
-round(sum(Donor_G22.P_Tot_Total*Feat.Feat_Prop),1) as PUnPCCT11
+round(sum(Donor_G22.P_Tot_CF_Total*Feat.Feat_Prop),1) as PUnPCC16,
+round(sum(Donor_G22.P_Tot_Total*Feat.Feat_Prop),1) as PUnPCCT16
 FROM [feat_aust_57km_sa1_16] as Feat 
 INNER JOIN [2016Census_G18_AUS_SA1] as Donor_G18 
 ON Donor_G18.SA1_7DIGITCODE_2016=Feat.Feat_Code  
