a9695e8966f4c68a50c9e75c37df2954c027de81
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-27 09:01:22 +1100

    Update feat_aust_11_places.sql

--- sql/feat_aust_11_places.sql
+++ sql/feat_aust_11_places.sql
@@ -1,9 +1,9 @@
 SELECT Feat_Aust_11.Poly as Poly,
 Feat_Aust_11.Feat_Code as Feat_Code,
-Feat_Aust_11.Feat_Prop as Feat_Prop_Area,
+Feat_Aust_11.Feat_Prop as Feat_PropA,
 Count(*) as places,
 Feat_Aust_11.geometry
 FROM Feat_Aust_11 
-INNER JOIN Place
+LEFT JOIN Place
 ON ST_Within(Place.geometry,Feat_Aust_11.geometry) 
 GROUP BY Poly,Feat_Code
