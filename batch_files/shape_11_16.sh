a9d1e6e1171a7253db9528406be4c35b932a645e
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-29 15:35:03 +1100

    Update shape_11_16.sh

--- batch_files/shape_11_16.sh
+++ batch_files/shape_11_16.sh
@@ -1,2 +1,2 @@
-ogr2ogr ../shapefiles/shape_75km_11_16.shp '../vrt/shape_11_16.vrt' -dialect sqlite -sql @../sql/shape_11_16.sql
-ogr2ogr -f "CSV" ../csv/shape_75km_11_16.csv ../shapefiles/shape_75km_11_16.shp
+ogr2ogr ../shapefiles/shape_57km_11_16.shp '../vrt/shape_11_16.vrt' -dialect sqlite -sql @../sql/shape_11_16.sql
+ogr2ogr -f "CSV" ../csv/shape_57km_11_16.csv ../shapefiles/shape_57km_11_16.shp
