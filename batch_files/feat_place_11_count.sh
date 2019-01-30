3c642016b6bb19dc99128f2d520a34b10b495afd
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-30 12:14:09 +1100

    Update feat_place_11_count.sh

--- batch_files/feat_place_11_count.sh
+++ batch_files/feat_place_11_count.sh
@@ -1,2 +1,4 @@
-ogr2ogr ../shapefiles/feat_57km_11_place_count.shp '../vrt/shape_place.vrt' -dialect sqlite -sql @../sql/feat_aust_11_places.sql
-ogr2ogr -f "CSV" ../csv/feat_57km_11_place_count.csv ../shapefiles/feat_57km_11_place_count.shp
+sh dbshpfun.sh feat_aust_57km_sa1_11
+sh dbshpfun.sh shape_57km_place_count
+sh dbshpfun.sh gis_osm_places_free_1
+spatialite ../spatialite_db/db.sqlite < ../spatialite_db/shape_57km_place_11_count.txt
