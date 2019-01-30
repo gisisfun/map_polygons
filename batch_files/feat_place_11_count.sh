7a4097208be4e112274dc054639c3bb228375681
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-01-31 10:40:17 +1100

    Update feat_place_11_count.sh

--- batch_files/feat_place_11_count.sh
+++ batch_files/feat_place_11_count.sh
@@ -1,4 +1,3 @@
 sh dbshpfun.sh feat_aust_57km_sa1_11
-sh dbshpfun.sh shape_57km_place_count
 sh dbshpfun.sh gis_osm_places_free_1
 spatialite ../spatialite_db/db.sqlite < ../spatialite_db/feat_place_11_count.txt
