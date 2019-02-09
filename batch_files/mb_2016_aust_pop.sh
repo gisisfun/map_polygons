369d9bbcffec1196941a500160f55edb7c182035
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-02-09 13:23:08 +1100

    Add files via upload

---
+++ batch_files/mb_2016_aust_pop.sh
@@ -0,0 +1,13 @@
+ogr2ogr -f "ESRI Shapefile" ../shapefiles/MB_2016_AUST.shp ../shapefiles/MB_2016_NSW.shp
+
+ogr2ogr -f "ESRI Shapefile" -update -append ../shapefiles/MB_2016_AUST.shp ../shapefiles/MB_2016_QLD.shp
+ogr2ogr -f "ESRI Shapefile" -update -append ../shapefiles/MB_2016_AUST.shp ../shapefiles/MB_2016_VIC.shp
+ogr2ogr -f "ESRI Shapefile" -update -append ../shapefiles/MB_2016_AUST.shp ../shapefiles/MB_2016_SA.shp
+ogr2ogr -f "ESRI Shapefile" -update -append ../shapefiles/MB_2016_AUST.shp ../shapefiles/MB_2016_WA.shp
+ogr2ogr -f "ESRI Shapefile" -update -append ../shapefiles/MB_2016_AUST.shp ../shapefiles/MB_2016_TAS.shp
+ogr2ogr -f "ESRI Shapefile" -update -append ../shapefiles/MB_2016_AUST.shp ../shapefiles/MB_2016_NT.shp
+ogr2ogr -f "ESRI Shapefile" -update -append ../shapefiles/MB_2016_AUST.shp ../shapefiles/MB_2016_ACT.shp
+ogr2ogr -f "ESRI Shapefile" -update -append ../shapefiles/MB_2016_AUST.shp ../shapefiles/MB_2016_OT.shp
+
+ogr2ogr ../shapefiles/mb_2016_aust_pop.shp '../vrt/mb_2016_aust_pop.vrt' -dialect sqlite -sql @../sql/mb_2016_aust_pop.sql
+
