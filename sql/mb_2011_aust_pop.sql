1b67fe575b1f92816660d167456cc9a4b7df0989
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-02-16 21:40:58 +1100

    Add files via upload

--- sql/mb_2011_aust_pop.sql
+++ sql/mb_2011_aust_pop.sql
@@ -1,7 +1,6 @@
 SELECT 
 MB.MB_CODE11 as MB_CODE11, 
 CAST(mb_counts.Persons_Usually_Resident AS INT) as Persons, 
-CAST((ST_Area(MB.geometry)*12396.3) AS FLOAT) as MB_Area,
 MB.geometry
 FROM MB
 INNER JOIN MB_Counts
--- sql/mb_2016_aust_pop.sql
+++ sql/mb_2016_aust_pop.sql
@@ -1,7 +1,6 @@
 SELECT 
 MB.MB_CODE16 as MB_CODE16, 
 CAST(mb_counts.Person AS INT) as Persons, 
-(ST_Area(MB_.geometry)*12391.3) as MB_Area,
 MB.geometry
 FROM MB
 INNER JOIN MB_Counts
