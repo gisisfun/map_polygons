36a510cffc6bdb5ea37947e8faffadd7a7adeaf9
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-05 23:00:15 +1100

    Delete shape_agil_count.sh

--- batch_files/shape_agil_count.sh
+++
@@ -1,4 +0,0 @@
-ogr2ogr -f 'ESRI Shapefile' ../shapefiles/agil_4326.shp  ../geojson/AGIL.json 
-ogr2ogr -t_srs 'EPSG:4823' ../shapefiles/agil.shp ../shapefiles/agil_4326.shp 
-ogr2ogr ../shapefiles/shape_donor_feat_57km_agil_count.shp '../vrt/shape_agil.vrt' -dialect sqlite -sql @../sql/shape_agil_count.sql
-#ogr2ogr -f "CSV" ../csv/shqpe_donor_feat_75km_aust_11.csv ../shapefiles/shape_donor_feat_57km_aust_11.shp
