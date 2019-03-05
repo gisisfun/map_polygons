f40b568380e2eb3213653e327aace3fbd3a99585
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-05 23:03:27 +1100

    Delete shape_services_count.sql

--- sql/shape_services_count.sql
+++
@@ -1,6 +0,0 @@
-SELECT Shape.p as Poly,Count(*) as services,
-Shape.geometry
-FROM Shape,POI
-WHERE ST_Within(POI.geometry,Shape.geometry) and 
-(POI.fclass = 'school' OR POI.fclass= 'police' OR POI.fclass= 'doctors' OR POI.fclass= 'hospital' OR POI.fclass= 'fire_station')
-GROUP BY Poly
