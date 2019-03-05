951f83db1336390e8c067156fd008d8a82a3a5df
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-05 23:02:38 +1100

    Delete shape_place_count.sql

--- sql/shape_place_count.sql
+++
@@ -1,6 +0,0 @@
-SELECT Shape.p as Poly,Count(*) as places,
-Shape.geometry
-FROM Shape,Place
-WHERE ST_Within(Place.geometry,Shape.geometry) and 
-(Place.fclass = 'city' OR Place.fclass= 'town')
-GROUP BY Poly
