e6340b670d4229693cd67a64b7b19e5a353bb338
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-05 21:46:06 +1100

    Add files via upload

---
+++ batch_files/shape_non_abs_counts.sh
@@ -0,0 +1,11 @@
+sh dbshpfun.sh gis_osm_places_free_1 4823 db
+sh dbshpfun.sh gis_osm_roads_free_1 4823 db
+sh dbshpfun.sh gis_osm_pois_free_1 4823 db
+
+ogr2ogr -f 'ESRI Shapefile' ../shapefiles/agil_4326.shp  ../geojson/AGIL.json 
+ogr2ogr -t_srs 'EPSG:4823' ../shapefiles/agil.shp ../shapefiles/agil_4326.shp 
+sh dbshpfun.sh agil 4823 db
+
+sh dbshpfun.sh aust_hex_shape_57km 4823 db
+
+spatialite ../spatialite_db/db.sqlite < ../spatialite_db/shape_nonabs_counts.txt
