e2d16f2417528c3deae06cab7f8394f70fbcac81
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-05 13:22:43 +1100

    Update donor_feat_area_11_B18_B21_B22_csv.sh

--- batch_files/donor_feat_area_11_B18_B21_B22_csv.sh
+++ batch_files/donor_feat_area_11_B18_B21_B22_csv.sh
@@ -1,6 +1,5 @@
 sh dbcsvfun.sh 2011Census_B18_AUST_SA1_long db
 sh dbcsvfun.sh 2011Census_B21_AUST_SA1_long db
 sh dbcsvfun.sh 2011Census_B22B_AUST_SA1_long db
-sh dbshpfun.sh SA1_2011_AUST 4823 db 
-sh dbshpfun.sh aust_hex_shape_57km 4823 db 
+sh dbshpfun.sh feat_aust_57km_sa1_11 4823 db 
 spatialite ../spatialite_db/db.sqlite < ../spatialite_db/donor_feat_area_11_B18_B21_B22.txt
