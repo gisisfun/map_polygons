495249a03cb3a6084e8e6f217528c79a1adfec89
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-05 23:03:03 +1100

    Delete shape_road_count.sql

--- sql/shape_road_count.sql
+++
@@ -1,5 +0,0 @@
-SELECT Shape.p as Poly,Count(*) as roads,
-Shape.geometry
-FROM Shape,Road
-WHERE ST_Within(Road.geometry,Shape.geometry) or ST_Intersects(Road.geometry,Shape.geometry)
-GROUP BY Poly
