b20abc4802d085836d44f67c4e257da1a2d341ef
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-05 15:16:02 +1100

    Update all_sql.sh

--- batch_files/all_sql.sh
+++ batch_files/all_sql.sh
@@ -66,3 +66,5 @@
 #ogr2ogr ../shapefiles/shape_57km_place_11_16.shp '../vrt/shape_11_16.vrt' -dialect sqlite -sql @../sql/shape_11_16_place.sql
 #ogr2ogr -f "CSV" ../csv/shape_57km_place_11_16.csv ../shapefiles/shape_57km_place_11_16.shp
 
+#SELECT AddGeometryColumn('feat_aust_57km_sa1_16', 'Geometry', 4823, 'MULTIPOLYGON', 'XY');
+#COMMIT;
