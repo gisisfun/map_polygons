c8ea173672eadc7e926b94f284ef6e05b540794f
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-05 13:12:00 +1100

    Update all_sql.sh

--- batch_files/all_sql.sh
+++ batch_files/all_sql.sh
@@ -10,29 +10,35 @@
 #ogr2ogr ../shapefiles/feat_aust_57km_sa1_16.shp '../vrt/feat_aust_16.vrt' -dialect sqlite -sql @../sql/feat_aust_16.sql
 #ogr2ogr -f "CSV" ../csv/feat_aust_57km_sa1_16.csv ../shapefiles/feat_aust_57km_sa1_16.shp
 
-#donor_feat_area_11_B18_B21_B22_csv
+#donor_feat_area_11_B18_B21_B22_csv (area weighting of 2011 Census data for 2011 ABS SA1 geography)
 #sh dbcsvfun.sh 2011Census_B18_AUST_SA1_long db
 #sh dbcsvfun.sh 2011Census_B21_AUST_SA1_long db
 #sh dbcsvfun.sh 2011Census_B22B_AUST_SA1_long db
-#sh dbcsvfun.sh feat_aust_57km_sa1_11 db
+#sh dbshpfun.sh feat_aust_57km_sa1_11 4823 db 
 #spatialite ../spatialite_db/db.sqlite < ../spatialite_db/donor_feat_area_11_B18_B21_B22.txt
 
-#donor_feat_area_16_G18_G21_G22_csv
+#donor_feat_area_16_G18_G21_G22_csv (area weighting of 2016 Census data for 2016 ABS SA1 geography)
 #sh dbcsvfun.sh 2016Census_G18_AUS_SA1 db
 #sh dbcsvfun.sh 2016Census_G21_AUS_SA1 db
 #sh dbcsvfun.sh 2016Census_G22B_AUS_SA1 db
-#sh dbcsvfun.sh feat_aust_57km_sa1_16 db
+##sh dbshpfun.sh feat_aust_57km_sa1_16 4823 db 
 #spatialite ../spatialite_db/db.sqlite < ../spatialite_db/donor_feat_area_16_G18_G21_G22.txt
 
-#feat_place_11_count
+#donor_feat_place_11_B18_B21_B22_csv (place weighting of 2011 Census data for 2011 ABS SA1 geography)
+#sh dbcsvfun.sh 2011Census_B18_AUST_SA1_long db
+#sh dbcsvfun.sh 2011Census_B21_AUST_SA1_long db
+#sh dbcsvfun.sh 2011Census_B22B_AUST_SA1_long db
 #sh dbshpfun.sh feat_aust_57km_sa1_11 4823 db
 #sh dbshpfun.sh gis_osm_places_free_1 4823 db
-#spatialite ../spatialite_db/db.sqlite < ../spatialite_db/feat_place_11_count.txt
+#spatialite ../spatialite_db/db.sqlite < ../spatialite_db/donor_feat_place_11_B18_B21_B22.txt
 
-#feat_place_16_count
+#donor_feat_area_16_G18_G21_G22_csv (place weighting of 2016 Census data for 2016 ABS SA1 geography)
+#sh dbcsvfun.sh 2016Census_G18_AUS_SA1 db
+#sh dbcsvfun.sh 2016Census_G21_AUS_SA1 db
+#sh dbcsvfun.sh 2016Census_G22B_AUS_SA1 db
 #sh dbshpfun.sh feat_aust_57km_sa1_16 db
 #sh dbshpfun.sh gis_osm_places_free_1 db
-#spatialite ../spatialite_db/db.sqlite < ../spatialite_db/feat_place_16_count.txt
+#spatialite ../spatialite_db/db.sqlite < ../spatialite_db/donor_feat_place_16_G18_G21_G22.txt
 
 #shape_place_count
 #ogr2ogr ../shapefiles/shape_57km_place_count.shp '../vrt/shape_place.vrt' -dialect sqlite -sql @../sql/shape_place_count.sql
