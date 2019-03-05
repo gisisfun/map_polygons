e521b5c6293c912499e018c649106c8df81a1d43
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-05 13:44:38 +1100

    Update donor_feat_place_16_G18_G21_G22.sh

--- batch_files/donor_feat_place_16_G18_G21_G22.sh
+++ batch_files/donor_feat_place_16_G18_G21_G22.sh
@@ -1,6 +1,6 @@
 sh dbcsvfun.sh 2016Census_G18_AUS_SA1 db
 sh dbcsvfun.sh 2016Census_G21_AUS_SA1 db
 sh dbcsvfun.sh 2016Census_G22B_AUS_SA1 db
-sh dbcsvfun.sh feat_aust_57km_sa1_16 db
-sh dbcsvfun.sh feat_57km_16_place_wt db
+sh dbshpfun.sh feat_aust_57km_sa1_16 4823 db 
+sh dbshpfun.sh gis_osm_places_free_1 4823 db
 spatialite ../spatialite_db/db.sqlite < ../spatialite_db/donor_feat_place_16_G18_G21_G22.txt
