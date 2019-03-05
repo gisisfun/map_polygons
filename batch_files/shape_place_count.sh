ae4b19d6ab5f6cb513469623105e927508bf1b99
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-05 22:59:36 +1100

    Delete shape_place_count.sh

--- batch_files/shape_place_count.sh
+++
@@ -1,2 +0,0 @@
-ogr2ogr ../shapefiles/shape_57km_place_count.shp '../vrt/shape_place.vrt' -dialect sqlite -sql @../sql/shape_place_count.sql
-#ogr2ogr -f "CSV" ../csv/shqpe_donor_feat_75km_aust_11.csv ../shapefiles/shape_donor_feat_57km_aust_11.shp
