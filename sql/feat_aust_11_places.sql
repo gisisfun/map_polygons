c2ccee530b2d6a04f9f7307b34acbd968748f660
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-08 12:22:00 +1100

    Delete feat_aust_11_places.sql

--- sql/feat_aust_11_places.sql
+++
@@ -1,9 +0,0 @@
-SELECT Feat_Aust_11.Poly as Poly,
-Feat_Aust_11.Feat_Code as Feat_Code,
-Feat_Aust_11.Feat_Prop as Feat_PropA,
-Count(*) as places,
-Feat_Aust_11.geometry
-FROM Feat_Aust_11 
-LEFT JOIN Place
-ON ST_Within(Place.geometry,Feat_Aust_11.geometry) 
-GROUP BY Poly,Feat_Code
