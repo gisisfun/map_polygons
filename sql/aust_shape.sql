40cf559138de4afdb77e62d0390dc6ef1fd9ebf0
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-08 11:22:02 +1100

    Update aust_shape.sql

--- sql/aust_shape.sql
+++ sql/aust_shape.sql
@@ -1,1 +1,6 @@
-SELECT Shapes.*, st_area(Shapes.geometry)*12391.3 as area, Shapes.geometry FROM Shapes, Aust WHERE ST_Intersects(Shapes.geometry,Aust.geometry)
+SELECT Shape.*,
+st_area(Shape.geometry)*12391.3 as area,
+Shape.geometry 
+FROM Shape, Aust 
+WHERE 
+ST_Intersects(Shape.geometry,Aust.geometry)
