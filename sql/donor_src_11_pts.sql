1d2b207f6136838702a64ce22537caab64b4f0d7
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-08 12:19:34 +1100

    Delete donor_src_11_pts.sql

--- sql/donor_src_11_pts.sql
+++
@@ -1,4 +0,0 @@
-SELECT CAST(Src.SA1_7DIG11 AS INT) as SA1_7DIG11, CAST(Src.SA1_MAIN11 AS INT) as SA1_MAIN11, ST_GeneratePoints(Src.geometry,CAST(Donor_B21.Persons_Total_Provided_unpaid_assistance AS INT)),
-'UnpAssist' as Type
-FROM Donor_B21,Src
-WHERE Src.SA1_7DIG11=Donor_B21.region_id
