b2b34b46ec004e088c1e40adb70da3deacff7ba8
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-08 12:06:47 +1100

    Update feat_aust_16.sql

--- sql/feat_aust_16.sql
+++ sql/feat_aust_16.sql
@@ -1,12 +1,12 @@
 SELECT 
  'R' as Feat_Type,
 'SA1_2016' as Feat_Src,
-CAST(Feat.SA1_7DIG16 AS INT) as Feat_Code,
+CAST(SA1_16.SA1_7DIG16 AS INT) as Feat_Code,
 '' as Feat_Name,
-Aust.p as Poly,  
-(ST_Area(ST_Intersection(Feat.geometry,Aust.geometry))*12391.3)/(ST_Area(Feat.geometry)*12391.3) as Feat_Prop,  
-ST_Intersection(Feat.geometry,Aust.geometry) 
-FROM Feat,Aust 
-WHERE ST_Intersects(Feat.geometry,Aust.geometry) 
+Shape_Aust.p as Poly,  
+(ST_Area(ST_Intersection(SA1_16.geometry,Shape_Aust.geometry))*12391.3)/(ST_Area(SA1_16.geometry)*12391.3) as Feat_Prop,  
+ST_Intersection(SA1_16.geometry,Shape_Aust.geometry) 
+FROM SA1_16,Shape_Aust 
+WHERE ST_Intersects(SA1_16.geometry,Shape_Aust.geometry) 
 and Feat_Prop is not NULL
 ORDER by Feat_Code
