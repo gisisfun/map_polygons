e065211d8f84862eda35e611c9c1bfbda7533b7e
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-09 23:52:38 +1100

    Delete mb11toced16.sql

--- spatialite_db/mb11toced16.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-MB11.MB_CODE11,
-CED16.CED_CODE16,
-CED16.CED_NAME16,
-ST_Area(ST_Intersection(MB11.geometry,CED16.geometry))/ST_Area(MB11.geometry) as Area_Prop
-FROM MB11,CED16
-WHERE ST_Intersects(MB11.geometry,CED16.geometry)
