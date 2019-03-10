f51074cd6350f143ec55142725689fef71038285
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-10 11:51:06 +1100

    Delete dbshpfun.sh

--- batch_files/dbshpfun.sh
+++
@@ -1,9 +0,0 @@
-
-sed -e "s#changeme1#${1}#g; s#changeme2#${2}#g; s#changeme3#${3}#g" ../spatialite_db/shpcmds_tmpl.txt >../spatialite_db/cmds.txt
-cat ../spatialite_db/cmds.txt | spatialite ../spatialite_db/${3}.sqlite
-
-
-
-
-
-
