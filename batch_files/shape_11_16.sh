338d431bcb0a3dfd341582762b6564565742291e
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-10 11:58:07 +1100

    Delete shape_11_16.sh

--- batch_files/shape_11_16.sh
+++
@@ -1,2 +0,0 @@
-ogr2ogr ../shapefiles/shape_57km_11_16.shp '../vrt/shape_11_16.vrt' -dialect sqlite -sql @../sql/shape_11_16.sql
-ogr2ogr -f "CSV" ../csv/shape_57km_11_16.csv ../shapefiles/shape_57km_11_16.shp
