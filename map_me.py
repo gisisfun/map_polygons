71d457feb6735f4292c7144f7c9af7f5b756df1c
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-17 22:01:30 +1100

    Update map_me.py

--- map_me.py
+++ map_me.py
@@ -2,7 +2,7 @@ import geopandas as gpd
 import numpy as np
 import matplotlib.pyplot as plt
 
-shp_path = "Downloads/map_polygons-master/shapefiles/shape_57km_place_11_16.shp"
+shp_path = "shapefiles/shape_57km_place_11_16.shp"
 sf = gpd.read_file(shp_path)
 print(len(sf))
 #sf.plot()
@@ -14,5 +14,5 @@ sf = sf.sort_values(['rel_need_for_assistance'], ascending = [1])
 sf=sf[sf.replace([np.inf, -np.inf], np.nan).notnull().all(axis=1)]  # .astype(np.float64)
 print(sf['rel_need_for_assistance'])
 #sf.plot(column='NeedA16').set_title("Hexagons. Default view");
-sf.plot(column='rel_need_for_assistance').set_title("Hexagons. Default view");
+sf.plot(column='rel_need_for_assistance', scheme = 'quantiles').set_title("Hexagons. Default view");
 plt.show()
