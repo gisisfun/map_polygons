53164bea759aa7152f7c7c10de31d9c16017d239
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-20 16:07:08 +1100

    Update map_me.py

--- map_me.py
+++ map_me.py
@@ -10,7 +10,7 @@ sf = sf[(sf.rel_need_for_assistance!=9999)]
 sf = sf.sort_values(['rel_need_for_assistance'], ascending = [1])
 sf=sf[sf.replace([np.inf, -np.inf], np.nan).notnull().all(axis=1)]  # .astype(np.float64)
 plt.rcParams["figure.figsize"] = (10,6)
-sf.plot(column='rel_need_for_assistance', scheme = 'quantiles', linewidth=0,cmap='Reds',legend=True).set_title("2016 to 2011 Relative Change in Need For Assistance (Quantiles)") ;
+sf.plot(column='rel_need_for_assistance', scheme = 'quantiles', k=5, linewidth=0,cmap='Reds',legend=True).set_title("2016 to 2011 Relative Change in Need For Assistance (Quantiles)") ;
 plt.annotate('Source: ABS Census of Population and Housing, 2016 and 2011',xy=(0.1, .08),  xycoords='figure fraction', horizontalalignment='left', verticalalignment='top', fontsize=12, color='#555555')
 plt.axis('off')
 plt.savefig('images/rel_need_for_assistance.png',bbox_inches='tight')
