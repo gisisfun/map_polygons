a2e826a9e9b9ec2abcbe926d81c2676fdd8e3a26
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-31 11:43:26 +1100

    Delete mb_2011_aust_pop.sql

--- sql/mb_2011_aust_pop.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-MB.MB_CODE11 as MB_CODE11, 
-CAST(mb_counts.Persons_Usually_Resident AS INT) as Persons, 
-MB.geometry
-FROM MB
-INNER JOIN MB_Counts
-ON MB.MB_CODE11=MB_Counts.Mesh_Block_ID
