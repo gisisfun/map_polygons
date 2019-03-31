f2cf24570812e01839eff3ffa73ebdefe9a04f43
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-03-31 11:43:37 +1100

    Delete mb_2011_aust_pop_.sql

--- sql/mb_2011_aust_pop_.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-MB_.MB_CODE11 as MB_CODE11, 
-CAST(MB_.Persons AS INT) as Persons, 
-CAST(ST_Area(MB_.geometry)*12391.3 AS FlOAT) as MB_Area,
-MB_.geometry
-FROM MB_
-WHERE MB_Area IS NOT NULL
