a861cff5d32bc7a310c64e949f3bf600ae29e8ba
gisisfun <45181329+gisisfun@users.noreply.github.com>
2019-04-01 14:27:31 +1100

    Update shape_11_16_area.sql

--- sql/shape_11_16_area.sql
+++ sql/shape_11_16_area.sql
@@ -1,39 +1,25 @@
-SELECT CAST(Shape_Area11.Poly as INT) as Poly,
-CAST(Shape_Area11.NeedA11 as FLOAT) as NeedA11,
-CAST(Shape_Area11.NeedAT11 as FLOAT) as NeedAT11,
-CAST(Shape_Area11.PUnPA11 as FLOAT) as PUnPA11,
-CAST(Shape_Area11.PUnPAT11 as FLOAT) as PUnPAT11,
-CAST(Shape_Area11.PUnPCC11 as FLOAT) as PUnPCC11,
-CAST(Shape_Area11.PUnPCCT11 as FLOAT) as PUnPCC11,
-CAST(Shape_Area16.NeedA16 as FLOAT) as NeedA16,
-CAST(Shape_Area16.NeedAT16 as FLOAT) as NeedAT16,
-CAST(Shape_Area16.PUnPA16 as FLOAT) as PUnPA16,
-CAST(Shape_Area16.PUnPAT16 as FLOAT) as PUnPAT16,
-CAST(Shape_Area16.PUnPCC16 as FLOAT) as PUnPCC16,
-CAST(Shape_Area16.PUnPCCT16 as FLOAT) as PUnPCCT16,
-place.places,
-AGIL.AGILplaces,
-service.services,
-bstation.bstations,
-road.roads,
-MBSP.MBSPplaces,
-Shape_Aust.est_area,
+SELECT CAST(Tabular_Area_Data.Poly as INT) as Poly,
+CAST(Tabular_Area_Data.NeedA11 AS FLOAT) as NeedA11,
+CAST(Tabular_Area_Data.NeedAT11 AS FLOAT) as NeedAT11,
+CAST(Tabular_Area_Data.PUnPA11 AS FLOAT) as NeedPA11,
+CAST(Tabular_Area_Data.PUnPAT11 AS FLOAT) as NeedPAT11,
+CAST(Tabular_Area_Data.PUnPCC11 AS FLOAT) as NeedPCC11,
+CAST(Tabular_Area_Data.PUnPCCT11 AS FLOAT) as NeedPCCT11,
+CAST(Tabular_Area_Data.NeedA16 AS FLOAT) as NeedA16,
+CAST(Tabular_Area_Data.NeedAT16 AS FLOAT) as NeedAT16,
+CAST(Tabular_Area_Data.PUnPA16 AS FLOAT) as NeedPA16,
+CAST(Tabular_Area_Data.PUnPAT16 AS FLOAT) as NeedPAT16,
+CAST(Tabular_Area_Data.PUnPCC16 AS FLOAT) as NeedPCC16,
+CAST(Tabular_Area_Data.PUnPCCT16 AS FLOAT) as NeedPCCT16,
+Tabular_Area_Data.Areas,
+Tabular_Area_Data.AGILAreas,
+Tabular_Area_Data.services,
+Tabular_Area_Data.bstations,
+Tabular_Area_Data.roads,
+Tabular_Area_Data.MBSPAreas,
+Tabular_Area_Data.est_area,
 Shape_Aust.geometry
 FROM Shape_Aust 
-LEFT JOIN Shape_Area11
-ON Shape_Aust.p=CAST(Shape_Area11.Poly as INT)
-LEFT JOIN Shape_Area16
-ON Shape_Aust.p=CAST(Shape_Area16.Poly as INT)
-LEFT JOIN MBSP 
-ON Shape_Aust.p = CAST(MBSP.Poly as INT)
-LEFT JOIN bstation
-ON Shape_Aust.p = CAST(bstation.Poly as INT)
-LEFT JOIN road
-ON Shape_Aust.p = CAST(road.Poly as INT)
-LEFT JOIN service
-ON Shape_Aust.p = CAST(service.Poly as INT)
-LEFT JOIN place
-ON Shape_Aust.p = CAST(place.Poly as INT)
-LEFT JOIN AGIL
-ON Shape_Aust.p = CAST(AGIL.Poly as INT)
+LEFT JOIN Tabular_Area_Data
+ON Shape_Aust.p=CAST(Tabular_Area_Data.Poly as INT)
 
