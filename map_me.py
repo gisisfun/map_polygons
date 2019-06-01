16f7c5c705c641aee4bd0b03a5466a21f4253ed7
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2019-06-01 20:46:58 +1000

    Add files via upload

--- map_me.py
+++ map_me.py
@@ -3,9 +3,22 @@ import numpy as np
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
+import sys
+import os
 
 def map_data(shape,size):
-   shp_path = "shapefiles/{shape}_{size}km_place_11_16.shp".format(shape=shape,size=size)
+   my_os=os.name
+   if (my_os is 'posix'):
+        cmd_text='/usr/bin/ogr2ogr'
+        slash='/'
+   else:
+        cmd_text='c:\\OSGeo4W64\\bin\\ogr2ogr.exe'
+        slash='\\'
+        gdal_vars = {'GDAL_DATA': 'C:\OSGeo4W64\share\gdal'}
+        os.environ.update(gdal_vars)
+
+        
+   shp_path = "shapefiles{slash}{shape}_{size}km_place_11_16.shp".format(shape=shape,slash=slash,size=size)
    sf = gpd.read_file(shp_path)
    sf['rel_need_for_assistance'] = ((sf.NeedA16-sf.NeedA11)/(sf.NeedAT16-sf.NeedAT11))*1
    sf = sf.fillna(9999)
@@ -16,7 +29,8 @@ def map_data(shape,size):
    sf.plot(column='rel_need_for_assistance', scheme = 'quantiles', k=5, linewidth=0,cmap='Reds',legend=True).set_title("2016 to 2011 Relative Change in Need For Assistance (Quantiles)") ;
    plt.annotate('Source: ABS Census of Population and Housing, 2016 and 2011',xy=(0.1, .08),  xycoords='figure fraction', horizontalalignment='left', verticalalignment='top', fontsize=12, color='#555555')
    plt.axis('off')
-   plt.savefig('images/rel_need_for_assistance.png',bbox_inches='tight')
+   print('hello')
+   plt.savefig('images{slash}rel_need_for_assistance.png'.format(slash=slash),bbox_inches='tight')
    #plt.show()  
 
 print('Number of arguments: {0} arguments.'.format(len(sys.argv)))
