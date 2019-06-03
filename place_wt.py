de140884fd4c80400b440b67d2de6d950027daa1
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2019-06-03 15:44:01 +1000

    Update place_wt.py

--- place_wt.py
+++ place_wt.py
@@ -4,6 +4,7 @@ import pandas
 import sqlite3
 import subprocess
 
+
 def shape_and_size (dirname, file, shape, size, newfile):
     my_os = os.name
     if (my_os is 'posix'):
@@ -19,6 +20,7 @@ def shape_and_size (dirname, file, shape, size, newfile):
     outfile.write(outfiletext)
     outfile.close() 
     return outfiletext
+
 
 def do_spatialite (sqlfile, dbfile):
     my_os = os.name
@@ -55,15 +57,17 @@ def geojson_to_shp (geojsonfile,shapefile,srid):
 
 def sql_to_ogr (sqlfile, vrtfile, shapefile):
     print('sqlfile: {0} vrt: {1} shapefile: {2}'.format(sqlfile,vrtfile,shapefile))
-    if (os.name is 'posix'):
+    my_os = os.name
+    if (my_os is 'posix'):
         cmd_text='/usr/bin/ogr2ogr'
         slash='/'
     else:
         cmd_text='c:\\OSGeo4W\\bin\\ogr2ogr.exe'
         slash='\\'
-    shapefiles_text = 'shapefiles{slash}{shapefile}.shp'.format(shapefile=shapefile,slash=slash)
-    vrt_text = 'vrt{slash}{vrtfile}.vrt'.format(vrtfile=vrtfile,slash=slash)
-    sql_text = '@sql{slash}{sqlfile}.sql'.format(sqlfile=sqlfile,slash=slash)
+        
+    shapefiles_text = 'shapefiles{slash}{shapefile}.shp'.format(shapefile=shapefile, slash=slash)
+    vrt_text = 'vrt{slash}{vrtfile}.vrt'.format(vrtfile=vrtfile, slash=slash)
+    sql_text = '@sql{slash}{sqlfile}.sql'.format(sqlfile=sqlfile, slash=slash)
 
     shp_options = [cmd_text,'-f', 'ESRI Shapefile', shapefiles_text , vrt_text , '-dialect', 'sqlite','-sql', sql_text ]
     #shp_options = [options_text]
@@ -74,6 +78,7 @@ def sql_to_ogr (sqlfile, vrtfile, shapefile):
     except FileNotFoundError:
         print('No files processed')
 
+        
 def sql_to_db (sqlfile,db):
     my_os = os.name
     if (my_os is 'posix'):
@@ -91,6 +96,7 @@ def sql_to_db (sqlfile,db):
         c.execute(sqltext)
         conn.commit()
 
+        
 def shp_to_db (filename,db,tblname,srid):
     os.environ['SPATIALITE_SECURITY']='relaxed'
     charset = 'CP1252'
@@ -111,6 +117,7 @@ def shp_to_db (filename,db,tblname,srid):
         c.execute(sql_statement)
         conn.commit()
 
+        
 def csv_to_db (filename, db, tblname):
     my_os = os.name
     if (my_os is 'posix'):
@@ -125,7 +132,8 @@ def csv_to_db (filename, db, tblname):
         df = pandas.read_csv('csv{slash}{filename}.csv'.format(filename=filename, slash=slash))
         df.to_sql(tblname, cnx)
 
-def cmds_to_db (cmdfile,db):
+        
+def cmds_to_db (cmdfile, db):
     my_os = os.name
     if (my_os is 'posix'):
         slash='/'
@@ -135,7 +143,6 @@ def cmds_to_db (cmdfile,db):
         cmd_text='c:\\OSGeo4W\bin\spatialite.exe'
     db_text = 'db{slash}{db}.sqlite'.format(db=db,slash=slash)
     cmd_text = 'vrt{slash}{cmdfile}.vrt'.format(cmdfile=cmdfile,slash=slash)
-
     options = [cmd_text,  db_text ,'<', cmd_text]
 
     try:
@@ -176,51 +183,51 @@ def process_sql(shape,size):
     # do_spatialite(sqlname,'db_place_{shape}_{size}'.format(shape=shape,size=size))
     
     print('aust_shape')
-    fname='aust_{shape}_shape_{size}km'.format(shape=shape,size=size)
-    sql_to_ogr('aust_shape','all',fname)
-    shp_to_db(fname,'db_place_{shape}_{size}'.format(shape=shape,size=size),fname,4823)
+    fname = 'aust_{shape}_shape_{size}km'.format(shape=shape, size=size)
+    sql_to_ogr('aust_shape', 'all', fname)
+    shp_to_db(fname,'db_place_{shape}_{size}'.format(shape=shape, size=size), fname, 4823)
     
     print('feat_aust_11_area')
-    fname='feat_aust_{size}km_sa1_11'.format(shape=shape,size=size)
-    sql_to_ogr('feat_aust_11','all_{shape}_{size}'.format(shape=shape,size=size),fname)
-    shp_to_db(fname,'db_place_{shape}_{size}'.format(shape=shape,size=size),fname,4823)
+    fname = 'feat_aust_{size}km_sa1_11'.format(shape=shape, size=size)
+    sql_to_ogr('feat_aust_11','all_{shape}_{size}'.format(shape=shape, size=size), fname)
+    shp_to_db(fname,'db_place_{shape}_{size}'.format(shape=shape, size=size), fname, 4823)
     
     print('feat_aust_16_area')
-    fname='feat_aust_{size}km_sa1_16'.format(shape=shape,size=size)
-    sql_to_ogr('feat_aust_16','all_{shape}_{size}'.format(shape=shape,size=size),fname)
-    shp_to_db(fname,'db_place_{shape}_{size}'.format(shape=shape,size=size),fname,4823)
+    fname = 'feat_aust_{size}km_sa1_16'.format(shape=shape, size=size)
+    sql_to_ogr('feat_aust_16','all_{shape}_{size}'.format(shape=shape, size=size), fname)
+    shp_to_db(fname,'db_place_{shape}_{size}'.format(shape=shape, size=size), fname, 4823)
     
     print('tabular_place_wt')   
-    csv_to_db('2011Census_B18_AUST_SA1_long','db_place_{shape}_{size}'.format(shape=shape,size=size),'2011Census_B18_AUST_SA1_long')
-    csv_to_db('2011Census_B21_AUST_SA1_long','db_place_{shape}_{size}'.format(shape=shape,size=size),'2011Census_B21_AUST_SA1_long')
-    csv_to_db('2011Census_B22B_AUST_SA1_long','db_place_{shape}_{size}'.format(shape=shape,size=size),'2011Census_B22B_AUST_SA1_long')
-    csv_to_db('2016Census_G18_AUS_SA1','db_place_{shape}_{size}'.format(shape=shape,size=size),'2016Census_G18_AUS_SA1')
-    csv_to_db('2016Census_G21_AUS_SA1','db_place_{shape}_{size}'.format(shape=shape,size=size),'2016Census_G21_AUS_SA1')
-    csv_to_db('2016Census_G22B_AUS_SA1','db_place_{shape}_{size}'.format(shape=shape,size=size),'2016Census_G22B_AUS_SA1')
-    fname='aust_{shape}_shape_{size}km'.format(shape=shape,size=size)
-    shp_to_db(fname,'db_place_{shape}_{size}'.format(shape=shape,size=size),fname,4823)
-    fname='feat_aust_{size}km_sa1_11'.format(shape=shape,size=size)
-    shp_to_db(fname,'db_place_{shape}_{size}'.format(shape=shape,size=size),fname,4823)
-    fname='feat_aust_{size}km_sa1_16'.format(shape=shape,size=size)
-    shp_to_db(fname,'db_place_{shape}_{size}'.format(shape=shape,size=size),fname,4823)
-    shp_to_db('gis_osm_places_free_1','db_place_{shape}_{size}'.format(shape=shape,size=size),'gis_osm_places_free_1',4823)
-    shp_to_db('gis_osm_roads_free_1','db_place_{shape}_{size}'.format(shape=shape,size=size),'gis_osm_roads_free_1',4823)
-    sql_to_ogr('shape_pois_shp','all','POI')
-    shp_to_db('POI','db_place_{shape}_{size}'.format(shape=shape,size=size),'POI',4823)
-    geojson_to_shp ('AGIL','agil',4823)
-    shp_to_db('agil','db_place_{shape}_{size}'.format(shape=shape,size=size),'agil',4823)
-    sql_to_ogr('shape_mbsp_shp','all','mbsp')
-    shp_to_db('mbsp','db_place_{shape}_{size}'.format(shape=shape,size=size),'mbsp',4823)
+    csv_to_db('2011Census_B18_AUST_SA1_long','db_place_{shape}_{size}'.format(shape=shape, size=size), '2011Census_B18_AUST_SA1_long')
+    csv_to_db('2011Census_B21_AUST_SA1_long','db_place_{shape}_{size}'.format(shape=shape, size=size), '2011Census_B21_AUST_SA1_long')
+    csv_to_db('2011Census_B22B_AUST_SA1_long','db_place_{shape}_{size}'.format(shape=shape, size=size), '2011Census_B22B_AUST_SA1_long')
+    csv_to_db('2016Census_G18_AUS_SA1','db_place_{shape}_{size}'.format(shape=shape, size=size), '2016Census_G18_AUS_SA1')
+    csv_to_db('2016Census_G21_AUS_SA1','db_place_{shape}_{size}'.format(shape=shape, size=size), '2016Census_G21_AUS_SA1')
+    csv_to_db('2016Census_G22B_AUS_SA1','db_place_{shape}_{size}'.format(shape=shape, size=size), '2016Census_G22B_AUS_SA1')
+    fname = 'aust_{shape}_shape_{size}km'.format(shape=shape, size=size)
+    shp_to_db(fname,'db_place_{shape}_{size}'.format(shape=shape, size=size), fname, 4823)
+    fname='feat_aust_{size}km_sa1_11'.format(shape=shape, size=size)
+    shp_to_db(fname,'db_place_{shape}_{size}'.format(shape=shape, size=size), fname, 4823)
+    fname='feat_aust_{size}km_sa1_16'.format(shape=shape, size=size)
+    shp_to_db(fname,'db_place_{shape}_{size}'.format(shape=shape, size=size), fname, 4823)
+    shp_to_db('gis_osm_places_free_1','db_place_{shape}_{size}'.format(shape=shape, size=size), 'gis_osm_places_free_1', 4823)
+    shp_to_db('gis_osm_roads_free_1','db_place_{shape}_{size}'.format(shape=shape, size=size), 'gis_osm_roads_free_1', 4823)
+    sql_to_ogr('shape_pois_shp', 'all', 'POI')
+    shp_to_db('POI','db_place_{shape}_{size}'.format(shape=shape, size=size), 'POI', 4823)
+    geojson_to_shp ('AGIL', 'agil', 4823)
+    shp_to_db('agil','db_place_{shape}_{size}'.format(shape=shape, size=size), 'agil', 4823)
+    sql_to_ogr('shape_mbsp_shp', 'all', 'mbsp')
+    shp_to_db('mbsp','db_place_{shape}_{size}'.format(shape=shape, size=size), 'mbsp', 4823)
 
-    sqlname='tabular_place_wt_{shape}_{size}.txt'.format(shape=shape,size=size)
-    shape_and_size ('spatialite_db','tabular_place_wt.txt',shape,size,sqlname)
-    do_spatialite(sqlname,'db_place_{shape}_{size}'.format(shape=shape,size=size))
+    sqlname='tabular_place_wt_{shape}_{size}.txt'.format(shape=shape, size=size)
+    shape_and_size ('spatialite_db', 'tabular_place_wt.txt', shape, size, sqlname)
+    do_spatialite(sqlname,'db_place_{shape}_{size}'.format(shape=shape, size=size))
     
     # spatialite ../spatialite_db/db.sqlite "vacuum;"
 
     print('shape_11_16_place')
-    fname='{shape}_{size}km_place_11_16'.format(shape=shape,size=size)
-    sql_to_ogr('shape_11_16_place','all_{shape}_{size}'.format(shape=shape,size=size),fname)
+    fname='{shape}_{size}km_place_11_16'.format(shape=shape, size=size)
+    sql_to_ogr('shape_11_16_place','all_{shape}_{size}'.format(shape=shape, size=size), fname)
 
 print('Number of arguments: {0} arguments.'.format(len(sys.argv)))
 print('Argument List: {0}'.format(str(sys.argv)))
