616c89d9a8a56aca07b6248521513e40c2dd32c9
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2019-07-18 15:43:08 +1000

    Update polygons.py

--- polygons.py
+++ polygons.py
@@ -122,76 +122,10 @@ def to_shp_tab(f_name,shape):
         # record the output!
         print('\nwriting {0} shapefile {1}_layer.shp'.format(shape, f_name))
         subprocess.check_call(shp_options)
-        write_vrt_file(f_name, shape, 'shp', 'ESRI Shapefile')
         print('\nwriting {0} shapefile {1}_layer.tab'.format(shape, f_name))
         subprocess.check_call(tab_options)
-        write_vrt_file(f_name, shape, 'tab', 'Mapinfo file')
     except FileNotFoundError:
         print('No files processed')
-
-
-def write_vrt_tabular_file(f_name):
-    my_os = os.name
-    if (my_os is 'posix'):
-        # cmd_text = '/usr/bin/ogr2ogr'
-        slash = '/'
-    else:
-        # cmd_text = 'c:\\OSGeo4W64\\bin\\ogr2ogr.exe'
-        slash = '\\'
-
-    vrt_template = """<OGRVRTDataSource>
-    <OGRVRTLayer name="{0}">
-        <SrcDataSource>{0}.csv</SrcDataSource>
-            <GeometryType>wkbPoint</GeometryType>
-            <LayerSRS>EPSG:4823</LayerSRS>
-            <GeometryField encoding="PointFromColumns" x="Long" y="Lat"/>
-        </OGRVRTLayer>
-    </OGRVRTDataSource>"""
-    vrt_content = vrt_template.format(f_name). replace(' ', '_')
-
-    print('\n tabular definition in vrt format for csv file to written \
-    to file: {0}.vrt'.format(f_name))
-    myfile = open('vrt{slash}{fname}.vrt'.format(fname=f_name
-    .replace(' ', '_'), slash=slash), 'w')
-    # open file for writing geojson layer
-    myfile.write(vrt_content)  # write vrt layer to open file
-    myfile.close()  # close file
-    # to check: ogrinfo -ro -al box_106km_layer.vrt
-    # to create australia layer
-    # https://gis.stackexchange.com/questions/147820/st-intersect-with-ogr2ogr-and-spatialite
-
-
-def write_vrt_file(f_name, shape, ext, ext_label):
-    my_os = os.name
-    if (my_os is 'posix'):
-        # cmd_text = '/usr/bin/ogr2ogr'
-        slash = '/'
-    else:
-        # cmd_text = 'c:\\OSGeo4W64\\bin\\ogr2ogr.exe'
-        slash = '\\'
-    vrt_template = """<OGRVRTDataSource>
-    <OGRVRTLayer name="shapes">
-        <SrcDataSource>{0}_layer.{1}</SrcDataSource>
-        <SrcLayer>{0}_layer</SrcLayer>
-        <GeometryType>wkbPolygon</GeometryType>
-        <LayerSRS>EPSG:4823</LayerSRS>
-    </OGRVRTLayer>
-    <OGRVRTLayer name="aust">
-        <SrcDataSource>AUS_2016_AUST.{1}</SrcDataSource>
-        <SrcLayer>AUS_2016_AUST</SrcLayer>
-        <GeometryType>wkbPolygon</GeometryType>
-        <LayerSRS>EPSG:4823</LayerSRS>
-    </OGRVRTLayer>
-</OGRVRTDataSource>"""
-    vrt_content = vrt_template.format(f_name, ext)
-
-    print('\n {0} vrt for {1} file to written to file: {2}_layer_{3}.vrt'
-    .format(shape, ext_label, f_name, ext))
-    myfile = open('vrt{slash}{fname}_layer_{extlabel}.vrt'
-    .format(slash=slash, fname=f_name, extlabel=ext_label
-    .replace(' ', '_')), 'w')  # open file for writing geojson layer
-    myfile.write(vrt_content)  # write vrt layer to open file
-    myfile.close()  # close file
 
 
 def ref_files():
@@ -325,8 +259,7 @@ def boxes(north, south, east, west, radial, outfile):
     layer_dict['Bounds']['Dataset']['West'] = tabular_df['W'].min()
     tabular_df.to_csv('csv{slash}{outfile}_dataset.csv'.format(outfile=
     outfile, slash = slash), sep = ',')
-    write_vrt_tabular_file('{outfile}_dataset'.format(outfile=outfile,
-    slash = slash))
+
 
     print('\n7/7 boxes json metadata to written to file: {0}_metadata.json'
     .format(outfile))
@@ -335,7 +268,6 @@ def boxes(north, south, east, west, radial, outfile):
     myfile.write(str(json.dumps(layer_dict)))
     #write geojson layer to open file
     myfile.close()  # close file
-    write_vrt_file(outfile, 'boxes', 'json', 'geojson')
     to_shp_tab(outfile, 'boxes')
     ref_files()
     print('\n')
@@ -531,7 +463,6 @@ def hexagons(north, south, east, west, radial, outfile):
         layer_dict['Bounds']['Dataset']['East'] = tabular_df['E'].max()
         layer_dict['Bounds']['Dataset']['West'] = tabular_df['W'].min()
         tabular_df.to_csv('csv{slash}{outfile}_dataset.csv'.format(outfile=outfile,slash=slash), sep=',', index = False)
-        write_vrt_tabular_file('{0}_dataset'.format(outfile))
 
         print('\n7/7 hexagons json metadata to written to file:\
          {0}_metadata.json'.format(outfile))
@@ -542,7 +473,6 @@ def hexagons(north, south, east, west, radial, outfile):
         #write geojson layer to open file
         myfile.close()  #close file
 
-        write_vrt_file(outfile, 'hexagons', 'json', 'geojson')
         to_shp_tab(outfile, 'hexagons')
         ref_files()
 
