c105cda9852356941984f5bd75c0cc7b32c53cd4
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2019-10-05 19:38:34 +1000

    Update Tiles.py

--- other_fun/isotiles/Tiles.py
+++ other_fun/isotiles/Tiles.py
@@ -5,20 +5,29 @@ import numpy as np
 class Fred:
     """
     Define Tile shape and extent
-
     North: maximum Latitude value 
     East: minimum longitude value
     South: minimum latitude value
     West: maximum longitude value
     Shape: either 'hex' or 'box'
-
-
     """
 
+
+    def dump(self):
+
+        print(self.North, 
+              self.East, 
+              self.South, 
+              self.West, 
+              self.Shape, 
+              self.Radial,
+              '\n')
+
+
     def __init__(self, Bounds_North: float = -8,
-                 Bounds_East: float = 96,
+                 Bounds_West: float = 96,
                  Bounds_South: float = -45,
-                 Bounds_West: float = 168,
+                 Bounds_East: float = 168,
                  Shape: str = 'hex',
                  Radial_km: float = 45):
         
@@ -28,23 +37,24 @@ class Fred:
         self.West = Bounds_West
         self.Shape = Shape
         self.Radial = Radial_km
-        if Shape is 'hex':
-            self.hor_seq = [0.7071,1,0.7071,1]
-            self.vert_seq = [0.7071,0.7071,0.7071,0.7071]
-            self.pattern = np.array( [[0,1,1,0],[1,0,0,1],[0,1,1,0],[1,0,0,1]])
-        else:
-            self.hor_seq = [1,1,1,1]
-            self.vert_seq = [1,1,1,1]
-            self.pattern = np.array( [[0,1,1,0],[1,0,0,1],[0,1,1,0],[1,0,0,1]])
 
-    def dump(self):
-        print(self.North, self.East, self.South, self.West, self.Shape, self.Radial)
+        if Shape is 'hex':
+            self.Hor_Seq = [0.7071,1,0.7071,1]
+            self.Vert_Seq = [0.7071,0.7071,0.7071,0.7071]
+            self.Pattern = np.array( [[0,1,1,0],[1,0,0,1],[0,1,1,0],[1,0,0,1]])
+        else:
+            self.Hor_Seq = [1,1,1,1]
+            self.Vert_Seq = [1,1,1,1]
+            self.Pattern = np.array( [[1,1,1,1],[1,0,1,0],[1,1,1,1],[1,0,1,0]])
 
 
     def horizontal(self):
-        #1/7 deriving vertical list of reference points from north to south for longitudes or x axis
+        """
+        horizontal function derives a list of vertical reference points from north to south for longitudes or x axis
+	"""
         angle = 180
         new_north = self.North
+        new_east = self.East
         #print(self.east,new_north,self.south,'\n')
         i = 0
         longitudes = []
@@ -54,11 +64,40 @@ class Fred:
             if i > 3:
                 i = 0
             
-            latlong = [new_north,self.East]
-            p = next_point(latlong,angle,self.Radial * self.hor_seq[i]) 
+            latlong = [new_north,new_east]
+            p = next_point(latlong,angle,self.Radial * self.Hor_Seq[i]) 
             new_north = p[0]
-            longitudes.append([[p[0],self.West],[p[0],self.East]])
+            new_east = p[1]
+            longitudes.append([[latlong[0],latlong[1]],[p[0],p[1]]])
             i += 1
+            
         return longitudes
-    
-        
+
+
+    def vertical(self):
+        """
+        vertical funtion derives a list of horizontal reference points from east to west for latitudes or y axis
+    	
+	"""
+
+        print('east {0} west {1}'.format(self.East,self.West))
+
+        angle = 90
+
+        i = 0
+        new_north = self.North
+        new_west = self.West
+        latitudes =[]
+        latitudes.append([[self.North,self.West],[self.South,self.East]])
+        while new_west <= self.East:
+            if i > 3:
+                i = 0
+
+            latlong = [new_north,new_west]
+            p = next_point(latlong,angle,self.Radial*self.Vert_Seq[i])
+            new_north = p[0]
+            new_west = p[1]
+            latitudes.append([[latlong[0],latlong[1]],[p[0],p[1]]])    
+            i += 1
+        return latitudes
+
