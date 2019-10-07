3ac58d5c9de35a29cba1b67c08f3886300e27f28
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2019-10-07 11:47:56 +1100

    Update Tools.py

--- other_fun/isotiles/Tools.py
+++ other_fun/isotiles/Tools.py
@@ -1,8 +1,29 @@
 from geopy.distance import distance,geodesic
+import os
 
 
 def next_point(coords, brng, radial):
     return geodesic(kilometers=radial).destination(point=coords, bearing=brng)
 
-    
-        
+            
+def to_geojson(geo_array):
+        .format(len(g_array)))
+        output_geojson = FeatureCollection(g_array)
+        # convert merged geojson features to geojson feature geohex_geojson
+    return output_geojosn
+
+
+def to_file(contents,filename):
+    my_os = os.name
+    if (my_os is 'posix'):
+        # cmd_text = '/usr/bin/ogr2ogr'
+        slash = '/'
+    else:
+        # cmd_text = 'c:\\OSGeo4W64\\bin\\ogr2ogr.exe'
+        slash = '\\'
+    print('writing geojson formatted hexagon dataset to file: {0}.json'.format(filename))
+    myfile = open(filename
+        .format(slash=slash), 'w')
+        #open file for writing geojson layer in geojson format
+        myfile.write(str(contents))  
+        myfile.close()  # close file
