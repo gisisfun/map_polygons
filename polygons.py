2c44c96c7eba1eb96d932c017a943653ef7c8b1b
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2019-10-16 19:11:23 +1100

    Update polygons.py

--- polygons.py
+++ polygons.py
@@ -14,23 +14,6 @@ import os
 def point_radial_distance(self, brng, radial):
     return geodesic(kilometers=radial).destination(point=self, bearing=brng)
 
-def line_intersectionnew(h_coords, v_coords):
-    #source: https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-between-two-lines-in-python
-    #xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
-    #ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1]) 
-    xdiff = (h_coords[0][0] - h_coords[1][0], v_coords[0][0] - v_coords[1][0])
-    ydiff = (h_coords[0][1] - h_coords[1][1], v_coords[0][1] - v_coords[1][1]) 
-    def det(a, b):
-        return a[0] * b[1] - a[1] * b[0]
-
-    div = det(xdiff, ydiff)
-    if div == 0:
-        raise Exception('lines do not intersect')
-    
-    d = (det(*h_coords), det(*v_coords))
-    x = det(d, xdiff) / div
-    y = det(d, ydiff) / div
-    return x, y
 
 def line_intersection(line1, line2):
     # source: https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-between-two-lines-in-python
