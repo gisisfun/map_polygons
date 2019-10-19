34dc6d718f6edfbe48226d8c7b6b6b9ec41a1509
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2019-10-20 07:18:42 +1100

    Add files via upload

--- class-polygons.py
+++ class-polygons.py
@@ -1,4 +1,25 @@
 from isotiles.thecode import test
+
+def random_points(bounds_n,bounds_s,bounds_e,bounds_w,numpoints):
+        
+    ns_range = bounds_n - bounds_s
+    ew_range = bounds_e - bounds_w
+    coord_list=[]
+
+    x_coords_list=[]
+    y_coords_list=[]
+    
+    for i in range(0,numpoints):
+        y_coord = bounds_s+random.randrange(0, ns_range*10000)/10000
+        x_coord = bounds_w+random.randrange(0, ew_range*10000)/10000
+        coord=[x_coord,y_coord]
+        coord_list.append(coord)
+        x_coords_list.append(x_coord)
+        y_coords_list.append(y_coord)
+        
+    print(layer_json)
+    return coord_list 
+
 #fred = test(north = -8, south = -45, east = 168, west = 96, radial = 45, shape ='hex')
 fred = test()
 
