5783ac4b60c555fc12ffecbab0e1bf35d16f7ce9
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2019-10-30 13:30:33 +1100

    Add files via upload

--- map_me.py
+++ map_me.py
@@ -1,18 +1,18 @@
 import sys
 from isotiles.thecode import Visual
 
-def do_map(theshape,theradial):
-    v = Visual(shape = theshape, radial = theradial)
+def do_map(theshape,theradial,theweight):
+    v = Visual(shape = theshape, radial = theradial, weight= theweight)
     v.map_data()
    
 print('Number of arguments: {0} arguments.'.format(len(sys.argv)))
 print('Argument List: {0}'.format(str(sys.argv)))
 if len(sys.argv) is 1:
-    (shape, size) = ['hex',57]
-    do_map(shape, size)
+    (shape, size, weight) = ['hex',57,'area']
+    do_map(shape, size, weight )
 else:
-    if (len(sys.argv) <3 ):
-        sys.exit("arguments are \nshape - hex or box \n size (in km) \n \npython3 map_me.py hex 57\n")
+    if (len(sys.argv) <4 ):
+        sys.exit("arguments are \nshape - hex or box \n size (in km)\nweight - area or place \n \npython3 map_me.py hex 57\n")
     else:
         (blah,shape,size) = sys.argv
         do_map(shape, size)
