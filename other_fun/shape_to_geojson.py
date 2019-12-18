39d41a634d6ae7d1dfa3e75dc2a5d73ac1269dc7
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2019-12-18 22:33:26 +1100

    Update shape_to_geojson.py

--- other_fun/shape_to_geojson.py
+++ other_fun/shape_to_geojson.py
@@ -1,4 +1,5 @@
 import shapefile
+
 class test():
     def me(self):
         sf = shapefile.Reader("shapefiles/SA3_2016_AUST") # , shapeType=3)
@@ -19,19 +20,23 @@ class test():
         #get the field names
         fields = sf.fields
         column_list = []
-        [column_list.append(x[0]) for x in fields]
+        [column_list.append(x[0]) for x in fields[1:]]
 
         # data values with null geography
         tab_data = sf.records()
 
         tab_data_val = []
+        row_ref = []
+        i = 0
         for tab_row in range(len(shapes_list_no_null)): # all rows
             #for column_name in column_list:
-            row_data = [shapes_list_no_null[tab_row][0]]
+            row_data = [] #shapes_list_no_null[tab_row][0]]
+            
             for tab_val in tab_data[shapes_list_no_null[tab_row][0]]: # each row of tab data (y)
-                #row_data.append(tab_row)
+                row_ref.append(i)
                 row_data.append(tab_val) #,len(shapes[tab_row].points)])
                 #column_dict[column_name] = str(tab_col)
+            i += 1 
             tab_data_val.append(row_data) # column_dict)
     
         geojson_properties_list = []
@@ -47,40 +52,22 @@ class test():
         parts_count = []
         points_len = []
         for i  in range(len(tab_data_val)):
-            parts_list.append(shapes[tab_data_val[i][0]].parts)
-            parts_count.append(len(shapes[tab_data_val[i][0]].parts))
-            points_len.append(len(shapes[tab_data_val[i][0]].points))
+            the_list = shapes[row_ref[i]].parts
+            parts_list.append(the_list)
+            the_list = shapes[row_ref[i]].parts
+            parts_count.append(len(shapes[row_ref[i]].parts))
+            points_len.append(len(shapes[row_ref[i]].points))
                 
         thing = []          
         for i in range(len(geojson_properties_list)):
-            thing.append([geojson_properties_list[i],parts_list[i],parts_count[i],points_len[i]])
-        
+            parts_list[i].append(points_len[i])
+            shapes_ref = row_ref[i]
+            #thing.append([geojson_properties_list[i],parts_count[i],parts_list[i]])
+            for j in range(0,parts_count[i]):
+                thing.append([geojson_properties_list[i],parts_count[i],shapes[shapes_ref].points[parts_list[i][j]:parts_list[i][j+1]]])
+        print(thing[100])
         return thing
             
             
 testing = test()
 fred = testing.me()
-#print(fred[0])
-#for i in range(len(parts_list)):
-#for pair in parts_list[1]:
-#    print(pair)
-#    print(len(
-        
-    
-
-
-#for subpolyptr in range(len(shapes[0].parts)-1):
-#    sub_coords = big_coords[shapes[0].parts[subpolyptr]:shapes[0].parts[subpolyptr+1]]
-    
-#records(self)
-#iterShapes(self)
-
-                points_len.append(len(geom_data.points))
-                
-                
-        return geojson_list
-            
-            
-testing = test()
-fred = testing.me()
-print(fred[0])
