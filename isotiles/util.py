fc798ec48d7848eeb12902956777904d37bb9623
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2019-12-18 16:35:14 +1100

    Update util.py

--- isotiles/util.py
+++ isotiles/util.py
@@ -246,35 +246,44 @@ class Util():
         # drop all array rows ith 0 parts
         shapes_list_no_null = [a for a in shapes_list_with_row_id if a[2] not in [0]]
 
-        #get the fields
+        #get the field names
         fields = sf.fields
         column_list = []
         [column_list.append(x[0]) for x in fields]
 
-
-        #for x in fields: column_dict[x[0]] = ''
+        # data values with null geography
         tab_data = sf.records()
+
         tab_data_val = []
         for tab_row in range(len(shapes_list_no_null)): # all rows
             #for column_name in column_list:
-            row_data = [0]
+            row_data = [shapes_list_no_null[tab_row][0]]
             for tab_val in tab_data[shapes_list_no_null[tab_row][0]]: # each row of tab data (y)
-                row_data.append(tab_val)
-            #column_dict[column_name] = str(tab_col)
+                #row_data.append(tab_row)
+                row_data.append(tab_val) #,len(shapes[tab_row].points)])
+                #column_dict[column_name] = str(tab_col)
             tab_data_val.append(row_data) # column_dict)
     
-        geojson_list = []
+        geojson_properties_list = []
         for tab_row in tab_data_val:
             dataset_dict_row = {}
-            for i in range(len(column_list)-1):
+            for i in range(0,len(column_list)):
+                #print(i)
                 dataset_dict_row[column_list[i]] = tab_row[i]
-            geojson_list.append(dataset_dict_row)
-        
-        # make a parts list
+            geojson_properties_list.append(dataset_dict_row)
+    
+        # make a parts list find numbers of points
         parts_list = []
-        for geom_data in shapes:
-            if len(geom_data.parts) > 0:
-                parts_list.append(geom_data.parts)
+        parts_count = []
+        points_len = []
+        for i  in range(len(tab_data_val)):
+            parts_list.append(shapes[tab_data_val[i][0]].parts)
+            parts_count.append(len(shapes[tab_data_val[i][0]].parts))
+            points_len.append(len(shapes[tab_data_val[i][0]].points))
+                
+        thing = []          
+        for i in range(len(geojson_properties_list)):
+            thing.append([geojson_properties_list[i],parts_list[i],parts_count[i],points_len[i]])
                 
         sf.close()
 
