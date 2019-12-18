a38c2358860335285801ea89f3702defab8fdc50
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2019-12-18 22:39:09 +1100

    Update shape_to_geojson.py

--- other_fun/shape_to_geojson.py
+++ other_fun/shape_to_geojson.py
@@ -64,7 +64,8 @@ class test():
             shapes_ref = row_ref[i]
             #thing.append([geojson_properties_list[i],parts_count[i],parts_list[i]])
             for j in range(0,parts_count[i]):
-                thing.append([geojson_properties_list[i],parts_count[i],shapes[shapes_ref].points[parts_list[i][j]:parts_list[i][j+1]]])
+                geojson_polygon = shapes[shapes_ref].points[parts_list[i][j]:parts_list[i][j+1]]
+                thing.append([geojson_properties_list[i],parts_count[i],geojson_polygon])
         print(thing[100])
         return thing
             
