6b8817e179f1bb532f674f8d89f7c3d345117e74
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2020-01-19 00:39:57 +0000

    Create yamltojson.py

---
+++ other_fun/yaml/yamltojson.py
@@ -0,0 +1,5 @@
+import yaml
+import json
+with open("example.yaml", 'r') as yaml_in, open("example.json", "w") as json_out:
+    yaml_object = yaml.safe_load(yaml_in) # yaml_object will be a list or a dict
+    json.dump(yaml_object, json_out)
