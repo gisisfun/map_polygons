05da3315a60edd0a075221152945488fc2e73406
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2020-05-06 21:45:29 +1000

    Update scrapy_datacamp.py

--- other_fun/scrapy_datacamp.py
+++ other_fun/scrapy_datacamp.py
@@ -16,7 +16,7 @@ css_per_topic_names = 'h4.dc-u-mb-4::text'
 css_per_topic_data = 'p.dc-u-mt-0::text' #first 12
 css_other_track = 'span.dc-dropdown--nav__track-name::text'
 css_track_comp = 'div div h5::text'
-css_track_names = 'div.track-block__main h4::text'
+#css_track_names = 'div.track-block__main h4::text'
 
 thefile = open('my_profile.txt','r')
 
@@ -29,18 +29,27 @@ dc_topic_names = sel.css(css_per_topic_names).extract()
 dc_topic_data = sel.css(css_per_topic_data).extract()[:12]
 
 # clean tex and convert to integer
-dc_topic_data = [int(re.sub('[\sA-Z]','',test)) for test in dc_topic_data]
+dc_topic_data = [int(re.sub('[\sA-Z]','',test)) 
+                 for test in dc_topic_data]
 
 my_topic_xp = pd.DataFrame(list(zip(dc_topic_names,dc_topic_data)), \
                columns =['Topic','XP'])
-
+# make category
 my_topic_xp.Topic = my_topic_xp.Topic.astype("category")
+my_topic_xp.sort_values(by='XP',ascending=False,inplace=True)
+
 
 plt.bar(x=my_topic_xp.Topic,height=my_topic_xp.XP)
 plt.xticks(rotation=60)
+plt.ylabel("XP")
+plt.xlabel("Topic")
 plt.title("Topics by XP")
+plt.savefig('topic_chart.png')
 plt.show()
 dc_counts = sel.css(css_dc_counts).extract()
+print('Total XP',dc_counts[0])
+print('Total Courses',dc_counts[1])
+print('Total Exercises',dc_counts[2])
 #xp = int(re.findall('.*[0-9].*',xp)[0]
 #.replace('XP','').replace(",","")
 #.strip())
@@ -64,6 +73,7 @@ my_courses.Course_Name = my_courses.Course_Name.str.title()
 my_courses.groupby('Technology', as_index=False)['Course_Name'].count().plot('Technology', kind='bar')
 plt.xticks(rotation=45)
 plt.title("Courses by Technology")
+plt.ylabel('Courses')
 plt.show()
 print(my_courses.groupby('Technology').count())
 print()
@@ -78,9 +88,24 @@ print(my_courses
       .sort_values().reset_index(drop=True))
 
 print("Skill Tracks")
-
+#
+css_track_names ='div.track-block__main'
 track_names = sel.css(css_track_names).extract()
 #print(track_names)
+track_names = [re.sub('     ',' ',re.sub(r'<.*>','',re.sub(r'<.*">','',test)))
+ for test in track_names]
+
+track_names = [''.join(x.split('\n')).
+               replace('  ',' ').
+               replace('  ',' ').
+               replace('  ',' ').
+               strip()  for x in track_names]
+
+my_tracks = pd.DataFrame(list(track_names), \
+               columns =['Skill_Track'])
+my_tracks.Skill_Track = my_tracks.Skill_Track.str.replace('\n ','').str.strip()
+print(my_tracks)
+
 
 track_comp = sel.css(css_track_comp).extract()
 #print(track_comp)
