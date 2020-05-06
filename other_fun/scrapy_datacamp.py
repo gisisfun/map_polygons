3513e50bd172ee15cd3d44587b53902ee06e01cc
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2020-05-06 17:39:38 +1000

    Update scrapy_datacamp.py

--- other_fun/scrapy_datacamp.py
+++ other_fun/scrapy_datacamp.py
@@ -2,48 +2,89 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Apr 23 21:58:02 2020
-
 @author: me
 """
 import pandas as pd
 from scrapy import Selector
 import matplotlib.pyplot as plt
-thefile = open('webpage.txt','r')
+import re
+
+xsel_lang_list = '//div[contains(@class,"course-block__technology course-block__technology--")]'
+css_course_list = 'h4.course-block__title::text'
+css_dc_counts = 'strong.dc-u-m-none::text'
+css_per_topic_names = 'h4.dc-u-mb-4::text' 
+css_per_topic_data = 'p.dc-u-mt-0::text' #first 12
+css_other_track = 'span.dc-dropdown--nav__track-name::text'
+css_track_comp = 'div div h5::text'
+css_track_names = 'div.track-block__main h4::text'
+
+thefile = open('my_profile.txt','r')
+
 html = thefile.read()
 sel = Selector(text=html)
 
-course_list = sel.css('h4.course-block__title::text').extract()
-xsel = '//div[contains(@class,"course-block__technology course-block__technology--")]'
-lang_list = sel.xpath(xsel).extract()
+print('XP by topic')
+#scape web page for content
+dc_topic_names = sel.css(css_per_topic_names).extract()
+dc_topic_data = sel.css(css_per_topic_data).extract()[:12]
+
+# clean tex and convert to integer
+dc_topic_data = [int(re.sub('[\sA-Z]','',test)) for test in dc_topic_data]
+
+my_topic_xp = pd.DataFrame(list(zip(dc_topic_names,dc_topic_data)), \
+               columns =['Topic','XP'])
+
+my_topic_xp.Topic = my_topic_xp.Topic.astype("category")
+
+plt.bar(x=my_topic_xp.Topic,height=my_topic_xp.XP)
+plt.xticks(rotation=60)
+plt.title("Topics by XP")
+plt.show()
+dc_counts = sel.css(css_dc_counts).extract()
+#xp = int(re.findall('.*[0-9].*',xp)[0]
+#.replace('XP','').replace(",","")
+#.strip())
+course_list = sel.css(css_course_list).extract()
+lang_list = sel.xpath(xsel_lang_list).extract()
 
 
-lang_list = [x.\
-             replace('<div class="course-block__technology course-block__technology--','').\
-             replace('"></div>','')for
- x in lang_list]
-print("length",len(course_list))
-for i,course in enumerate(course_list):
-    print(course_list[i])
-    
+lang_list = [re.sub('<.*.--','',re.sub('"></div>','',x)) for x in lang_list]
+#lang_list=[ test[len(test)-91:-8] for test in lang_list]
+
 my_courses = pd.DataFrame(list(zip(lang_list,course_list)), \
                columns =['Technology','Course_Name'])
-print('all courses')
-print(my_courses.groupby('Course_Name', as_index=False)['Technology'].count())
-print('technology by course count')
-print(my_courses.groupby('Technology', as_index=False)['Course_Name'].count())
+my_courses.Technology = my_courses.Technology.str.title()
+my_courses.Course_Name = my_courses.Course_Name.str.title()
+#print('all courses')
+
+#print(my_courses.groupby('Course_Name', as_index=False)['Technology'].count())
+#print('technology by course count')
+#print(my_courses.groupby('Technology', as_index=False)['Course_Name'].count())
 
 my_courses.groupby('Technology', as_index=False)['Course_Name'].count().plot('Technology', kind='bar')
+plt.xticks(rotation=45)
+plt.title("Courses by Technology")
 plt.show()
+print(my_courses.groupby('Technology').count())
+print()
+print('Python Courses')
+print(my_courses
+      .loc[my_courses.Technology=='Python','Course_Name']
+      .sort_values().reset_index(drop=True))
+print()
+print('R Courses')
+print(my_courses
+      .loc[my_courses.Technology=='R','Course_Name']
+      .sort_values().reset_index(drop=True))
 
 print("Skill Tracks")
-track_names = sel.css('div.track-block__main h4::text').extract()
-print(track_names)
-track_comp = sel.css('div div h5::text').extract()
-print(track_comp)
 
+track_names = sel.css(css_track_names).extract()
+#print(track_names)
 
-other=sel.css('span.dc-dropdown--nav__track-name::text').extract()
-print(other)
+track_comp = sel.css(css_track_comp).extract()
+#print(track_comp)
+
+other=sel.css(css_other_track).extract()
+#print(other)
 my_tech = my_courses.Technology.unique()
-python_courses = my_courses[my_courses.Technology=="python"]['Course_Name'].sort_values().reset_index(drop=True)
-r_courses = my_courses[my_courses.Technology=="r"]['Course_Name'].sort_values().reset_index(drop=True)
