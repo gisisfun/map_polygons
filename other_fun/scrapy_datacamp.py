c8e5e6dad1dc534f53e3c2b79ca6e7f00e5738f2
Damien Clarke <45181329+gisisfun@users.noreply.github.com>
2020-05-07 21:39:09 +1000

    Update scrapy_datacamp.py

--- other_fun/scrapy_datacamp.py
+++ other_fun/scrapy_datacamp.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Thu Apr 23 21:58:02 2020
@@ -9,7 +9,8 @@ from scrapy import Selector
 import matplotlib.pyplot as plt
 import re
 
-def just_words(raw_html):
+
+def just_words(raw_html,func,non_extra=[]):
     '''
     Finds all words in html 
     
@@ -19,22 +20,28 @@ def just_words(raw_html):
     returns:
         list of strings without html text
     '''
-    non_words = ['0', 'block__main', 'class', 
+    non_words = ['block__main', 'class', 
                  'dc', 'div', 'h4', 'mb',
                  'mt', 'p', 'track', 'u',
-                 'course','block__technology']
+                 'course','block__technology'] + non_extra
     final_list=[]
     for y in raw_html:
         words_l=[]
         html_words =re.findall('[\w]+',y)
         for x in html_words:
             if x not in non_words:
-                if x =='amp':
-                    words_l.append('&')
-                else:
-                    words_l.append(x)
+                words_l.append(func(x))
         final_list.append(' '.join(words_l))
     return final_list
+
+def nothing(value):
+    return value
+
+def is_amp(f_value):
+    output=f_value
+    if f_value=="amp":
+        output='&'   
+    return output
 
 xsel_lang_list = '//div[contains(@class,"course-block__technology course-block__technology--")]'
 css_course_list = 'h4.course-block__title::text'
@@ -54,7 +61,7 @@ print('XP by topic')
 #scape web page for content
 dc_topic_names = sel.css(css_per_topic_names).extract()
 dc_topic_data = sel.css(css_per_topic_data).extract()[:12]
-
+dc_topic_data = just_words(test,nothing,['XP'])
 # clean tex and convert to integer
 dc_topic_data = [int(re.sub('[\sA-Z]','',test)) 
                  for test in dc_topic_data]
@@ -81,8 +88,8 @@ print('Total Exercises',dc_counts[2])
 print()
 print('all courses')
 course_list = sel.css(css_course_list).extract()
-
-lang_list=just_words(sel.xpath(xsel_lang_list).extract())
+lang_raw=sel.xpath(xsel_lang_list).extract()
+lang_list=just_words(lang_raw,is_amp,['0'])
 
 my_courses = pd.DataFrame(list(zip(lang_list,course_list)), \
                columns =['Technology','Course_Name'])
@@ -111,7 +118,7 @@ print("Skill Tracks")
 css_track_names ='div.track-block__main'
 track_names = sel.css(css_track_names).extract()
 
-track_names = just_words(track_names)
+track_names = just_words(track_names,is_amp,['0'])
 my_tracks = pd.DataFrame(list(track_names), \
                columns =['Skill_Track'])
 my_tracks.Skill_Track = my_tracks.Skill_Track.str.replace('\n ','').str.strip()
